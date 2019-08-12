3dfce86430e5106c275d3f91ba621a353d91426b
Vance Leon <vanceleon44@gmail.com>
2019-08-12 12:02:06 -0400

    knexfile:

--- knexfile.js
+++ knexfile.js
@@ -7,3 +7,34 @@ const localPgConnection = {
 
 const dbConnection = process.env.DATABASE_URL || localPgConnection;
 
+module.exports = {
+    development: {
+        client: 'postgresql',
+        connection: {
+            database: process.env.dbConnection
+        },
+        useNullAsDefault: true,
+        migrations: {
+            directory: './routes/data/migrations'
+        },
+        seeds: {
+            directory: './routes/data/seeds'
+        },
+    },
+    production: {
+        client: 'pg',
+        connection: dbConnection,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+            directory: './routes/data/migrations'
+        },
+        seeds: {
+            directory: './routes/data/seeds'
+        },
+        useNullAsDefault: true
+    }
+};
