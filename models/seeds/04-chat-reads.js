00ba489109ad43e5e5a2e13d0f9180c47af53c49
Vance Leon <vanceleon44@gmail.com>
2019-08-13 12:09:30 -0400

    chatreads summary parts books edit

---
+++ models/seeds/04-chat-reads.js
@@ -0,0 +1,13 @@
+exports.seed = function(knex, Promise) {
+    return knex('chat_reads')
+    .del()
+    .then(function() {
+        return knex('chat_reads').insert([
+            {
+                user_id: 1,
+                book_id: 1,
+                review: 'This was a great read!',
+            }
+        ])
+    })
+}
