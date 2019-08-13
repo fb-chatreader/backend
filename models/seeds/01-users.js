431327f424da5cd0b29b1d8af51edc10d5ac3a8a
Vance Leon <vanceleon44@gmail.com>
2019-08-13 13:25:12 -0400

    uncommented users and added fb id

--- models/seeds/01-users.js
+++ models/seeds/01-users.js
@@ -1,15 +1,12 @@
-// exports.seed = function(knex, Promise) {
-//     // delete all existing entries
-//     return knex('users')
-//     .del()
-//     .then(function() {
-//         return knex('users').insert([
-//             {
-//                 facebook_id: 1,
-//             },
-//             {
-//                 facebook_id: 1,
-//             }
-//         ])
-//     })
-// }
+exports.seed = function(knex, Promise) {
+    // delete all existing entries
+    return knex('users')
+    .del()
+    .then(function() {
+        return knex('users').insert([
+            {
+                facebook_id: 10152368852405295,
+            },
+        ])
+    })
+}
