de5e49e329dc692c4099c40be8945f744227b12a
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-13 19:26:50 -0400

    Removes unnecssary comments

--- models/migrations/20190813185036_initial_table_fixes.js
+++ models/migrations/20190813185036_initial_table_fixes.js
@@ -1,13 +1,11 @@
 exports.up = function(knex) {
   return knex.schema.table('books', tbl => {
     tbl.dropColumn('user_id');
-    // tbl.timestamp('created_at').defaultTo(knex.fn.now());
   });
 };
 
 exports.down = function(knex) {
   return knex.schema.table('books', tbl => {
     tbl.dropColumn('created_at');
-    // tbl.timestamps('created_at');
   });
 };
