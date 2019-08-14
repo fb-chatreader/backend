ee61423447749872b8661a48413cdaec0aef3ce2
Vance Leon <vanceleon44@gmail.com>
2019-08-14 13:17:00 -0400

    postback is interacting with user again

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,175 +1,146 @@
 const express = require('express');
 const axios = require('axios');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 const router = express.Router().use(bodyParser.json());
-const request = require("request");
+const request = require('request');
 const db = require('../models/dbConfig');
 
+const URL = 'http://localhost:8000/api';
 
-function handleMessage(senderPsid, receivedMessage) {
-    let response;
-    // Checks if the message contains text
-    let attachmentUrl = receivedMessage.text;
-    if (attachmentUrl === 'book') {
-      axios.get('http://localhost:8000/api/books')
-      .then(function(res) {
-        response = {text: res.data}
-        // {
-        //   attachment: {
-        //     type: "template",
-        //     payload: {
-        //       template_type: "generic",
-        //       elements: [
-        //         {
-        //           title: "Is this the right picture?",
-        //           subtitle: "Tap a button to answer.",
-        //           image_url: attachmentUrl,
-        //           buttons: [
-        //             {
-        //               type: "postback",
-        //               title: "Yes!",
-        //               payload: "yes"
-        //             },
-        //             {
-        //               type: "postback",
-        //               title: "No!",
-        //               payload: "no"
-        //             }
-        //           ]
-        //         }
-        //       ]
-        //     }
-        //   }
-        // };
-        console.log("format response",response)
-        // console.log(attachmentUrl)
-      })
-      .catch(function(err) {
-        console.log(err)
-      })
-      
-      
-    } else if (receivedMessage.attachments) {
-      // Get the URL of the message attachment edge case if user puts attachment 
-      let attachmentUrl = receivedMessage.attachments[0].payload.url;
-      response = {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "generic",
-            elements: [
+function handleMessage(sender_psid, received_message) {
+  let response;
+  
+  // Checks if the message contains text
+  if (received_message.text) {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
               {
-                title: `Sorry can't receive attachments at this time. Please select valid input`,
-                subtitle: "Tap a button to answer.",
-                image_url: attachmentUrl,
-                buttons: [
-                  {
-                    type: "postback",
-                    title: "Continue",
-                    payload: "continue"
-                  },
-                  {
-                    type: "postback",
-                    title: "Next",
-                    payload: "next"
-                  }
-                ]
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
               }
-            ]
-          }
+            ],
+          }]
         }
-      };
+      }
     }
-    // Send the response message
-    callSendAPI(senderPsid, response);
-  }
+  } 
   
-  function handlePostback(senderPsid, receivedPostback) {
-    let response;
-    let payload = receivedPostback.payload;
-    if (payload === "yes") {
-      response = { text: "Thanks!" };
-    } else if (payload === "no") {
-      response = { text: "Oops, try sending another image." };
-    }
-    callSendAPI(senderPsid, response);
-  }
-  
-  function callSendAPI(senderPsid, response) {
-    let requestBody = {
-      recipient: {
-        id: senderPsid
-      },
-      message: response
-    };
+  // Send the response message
+  callSendAPI(sender_psid, response);    
+}
 
-    request(
-      {
-        uri: "https://graph.facebook.com/v2.6/me/messages",
-        qs: { access_token: process.env.ACCESS_TOKEN },
-        method: "POST",
-        json: requestBody
-      },
-      (err, res, body) => {
-        if (!err) {
-          console.log("message sent");
-        } else {
-          console.error("unable to send message" + err);
-        }
-      }
-    );
+function handlePostback(sender_psid, received_postback) {
+  let response;
+  
+  // Get the payload for the postback
+  let payload = received_postback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
   }
-  
-  router.post("/webhook", (req, res) => {
-    let body = req.body;
-    if (body.object === "page") {
-      body.entry.forEach(function(entry) {
-        let webhookEvent = entry.messaging[0];
-        console.log(webhookEvent);
-        // sender PSID
-        let senderPsid = webhookEvent.sender.id;
-        // reciever PSID
-        let receiverPsid = webhookEvent.recipient.id;
-  
-        if (webhookEvent.message) {
-          handleMessage(senderPsid, webhookEvent.message);
-        } else if (webhookEvent.postback) {
-          handlePostback(senderPsid, webhookEvent.postback);
-        }
-      });
-      res.status(200).send("Event received");
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    "recipient": {
+      "id": sender_psid
+    },
+    "message": response
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
     } else {
-      res.sendStatus(404);
+      console.error("Unable to send message:" + err);
     }
-  });
-  
-  router.get("/webhook", (req, res) => {
-    let VERIFY_TOKEN = process.env.VERIFY_TOKEN
-    let mode = req.query["hub.mode"];
-    let token = req.query["hub.verify_token"];
-    let challenge = req.query["hub.challenge"];
-  
-    if (mode && token) {
-      if (mode === "subscribe" && token === VERIFY_TOKEN) {
-        console.log("Webhook verified");
-        res.status(200).send(challenge);
-      } else {
-        res.sendStatus(403);
+  }); 
+}
+
+router.post('/webhook', (req, res) => {
+  let body = req.body;
+  if (body.object === 'page') {
+    body.entry.forEach(function(entry) {
+      let webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+      // sender PSID
+      let senderPsid = webhookEvent.sender.id;
+      // reciever PSID
+      let receiverPsid = webhookEvent.recipient.id;
+
+      if (webhookEvent.message) {
+        handleMessage(senderPsid, webhookEvent.message);
+      } else if (webhookEvent.postback) {
+        handlePostback(senderPsid, webhookEvent.postback);
       }
+    });
+    res.status(200).send('Event received');
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.get('/webhook', (req, res) => {
+  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('Webhook verified');
+      res.status(200).send(challenge);
     } else {
-      res.sendStatus(404);
+      res.sendStatus(403);
     }
-  });
-  
-  router.get("/", (req, res) => {
-    res.send("API RUNNING!!!");
-    res.status(200);
-  });
-  
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+// router.get('/', (req, res) => {
+//   res.send('API RUNNING!!!');
+//   res.status(200);
+// });
+
 //   const PORT = process.env.PORT || 5500;
 //   router.listen(PORT, () => {
 //     console.log(`Server running on port ${PORT}`);
 //   });
-  
 
 module.exports = router;
+
