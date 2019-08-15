1646f17047ec0b116bfda1888b5f6ae4d2f0f4ab
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-15 03:35:43 -0400

    Expands messenger routes to allow for modules.

---
+++ routes/messages/classes/Command.js
@@ -0,0 +1,33 @@
+const request = require('request');
+
+module.exports = class Command {
+  constructor(response, event) {
+    this.response = response;
+    this.sender = event.sender.id;
+  }
+
+  sendResponse() {
+    const request_body = {
+      recipient: {
+        id: this.sender
+      },
+      message: this.response
+    };
+
+    request(
+      {
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: request_body
+      },
+      (err, res, body) => {
+        if (!err) {
+          console.log('message sent!');
+        } else {
+          console.error('Unable to send message:' + err);
+        }
+      }
+    );
+  }
+};
