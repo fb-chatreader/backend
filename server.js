9599fe4b6e75b3aea98cf4f96ae175c5066b7d74
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-15 03:36:09 -0400

    Makes minor code style adjustments

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-"use strict";
-const express = require("express");
+'use strict';
+const express = require('express');
 const cors = require('cors');
 
 // route declarations
-const messageRouter = require('./routes/messageRoutes');
-const bookRouter = require('./routes/bookRoutes')
-const userRouter = require('./routes/userRoute')
-const summaryRouter = require('./routes/summaryPartRoute')
-const chatReadRouter = require('./routes/chatReadRoute')
+const messageRouter = require('./routes/messages/messageRoutes');
+const bookRouter = require('./routes/bookRoutes');
+const userRouter = require('./routes/userRoute');
+const summaryRouter = require('./routes/summaryPartRoute');
+const chatReadRouter = require('./routes/chatReadRoute');
 
 const server = express();
 server.use(cors());
@@ -18,10 +18,12 @@ server.use('/api/messenger', messageRouter);
 server.use('/api/books', bookRouter);
 server.use('/api/users', userRouter);
 server.use('/api/summaries', summaryRouter);
-server.use('/api/chatReads', chatReadRouter)
+server.use('/api/chatReads', chatReadRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json('Welcome to ChatReader server!');
 });
+
+server.get;
 
 module.exports = server;
