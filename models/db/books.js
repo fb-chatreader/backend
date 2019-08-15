4ce9ce6f3f587a69480aec738f6a108afc6041bf
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-15 03:37:05 -0400

    Adds console.log to notify of current build environment

---
+++ models/db/books.js
@@ -0,0 +1,35 @@
+const db = require('../dbConfig.js');
+
+module.exports = {
+  retrieve,
+  retrieveByID,
+  write,
+  edit,
+  remove
+};
+
+function retrieve() {
+  return db('books');
+}
+
+function retrieveByID(bkid) {
+  return db('books').where({ id: bkid });
+}
+
+function write(book) {
+  return db(`books`)
+    .where({ book })
+    .then(ids => ({ id: ids[0] }));
+}
+
+function edit(bkid, book) {
+  return db('books')
+    .where({ id: bkid })
+    .update(book);
+}
+
+function remove(bkid) {
+  return db('books')
+    .where({ id: bkid })
+    .del();
+}
---
+++ models/db/summaryParts.js
@@ -0,0 +1,35 @@
+const db = require('../dbConfig.js');
+
+module.exports = {
+  retrieve,
+  retrieveByID,
+  write,
+  edit,
+  remove
+};
+
+function retrieve() {
+  return db('summary_parts');
+}
+
+function retrieveByID(summaryid) {
+  return db('summary_parts').where({ id: summaryid });
+}
+
+function write(summary) {
+  return db(`summary_parts`)
+    .where({ summary })
+    .then(ids => ({ id: ids[0] }));
+}
+
+function edit(summaryid, summary) {
+  return db('summary_parts')
+    .where({ id: summaryid })
+    .update(summary);
+}
+
+function remove(summaryid) {
+  return db('users')
+    .where({ id: summaryid })
+    .del();
+}
