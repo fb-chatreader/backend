b7e331a45ef28c21494de87c8229891129a975c9
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-15 03:38:19 -0400

    Adds console.log to notify of build environment

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 
 const dbEnvironment = process.env.DB_ENVIRONMENT || 'development';
+console.log(`\n\nDeploying ${dbEnvironment} build\n\n`);
 const config = require('../knexfile')[dbEnvironment];
 
 module.exports = knex(config);
