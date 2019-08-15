20ed794dea278bc250837bede6cb383ba63aae20
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-15 19:35:28 -0400

    Added temporary logging solutions to FB policy issues

--- routes/messages/middleware.js
+++ routes/messages/middleware.js
@@ -1,9 +1,30 @@
-function verifyWebhook({ body }, res, next) {
-  if (body.object === 'page') {
-    next();
-  } else {
-    res.sendStatus(404);
+// IIFE to log enforcement issues
+const verifyWebhook = (enforcement => ({ body }, res, next) => {
+  try {
+    // Temporary measure to notice enforcement issues while I build a
+    // more long term solution
+    if (enforcement.length) {
+      enforcement.forEach(notice =>
+        console.error(
+          'ENFORCEMENT NOTICE: ',
+          notice.action,
+          ' - ',
+          notice.reason
+        )
+      );
+    }
+    if (body.object === 'page') {
+      if (body.entry[0].messaging[0]) {
+        next();
+      } else if (body.entry['policy-enforcement']) {
+        enforcement.push(body.entry['policy-enforcement']);
+      }
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    console.log('Webhook middleware error: ', err, body.entry);
   }
-}
+})([]);
 
 module.exports = { verifyWebhook };
