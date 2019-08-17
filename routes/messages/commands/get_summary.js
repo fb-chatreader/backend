20fe0445d45b37e3a934bad6d077fdfad90434a0
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-16 21:37:33 -0400

    Combines postback and commands into one folder

---
+++ routes/messages/commands/get_summary.js
@@ -0,0 +1,68 @@
+const Users = require('../../../models/db/users.js');
+const ChatReads = require('../../../models/db/chatReads.js');
+const Summaries = require('../../../models/db/summaryParts.js');
+
+// Query database to get current summary location
+// If there isn't one, create it
+// Otherwise, increment and get next summary (check for end of book)
+
+module.exports = async event => {
+  /* HARD CODED */
+  const user_id = 1; // await Users.retrieve({ facebook_id: event.sender.id }).first(); // then get the ID
+  /* HARD CODED */
+  const book_id = 1;
+  const chatread = await ChatReads.retrieve({ user_id, book_id }).first();
+  let current_part = chatread ? chatread.current_summary_id : null;
+
+  if (!current_part) {
+    /* INCOMPLETE */
+    // Create a new chat read in the database (only one right now so no need to code it)
+    current_part = 1;
+  }
+  const nextSummary = await Summaries.retrieve({
+    id: current_part + 1
+  }).first();
+  let text, next_part, buttons;
+
+  if (!nextSummary || nextSummary.book_id !== book_id) {
+    // We've reached the end of our database or the next summary is a different book
+    text =
+      'We hope you enjoyed the summary!  Would you like to find another book?';
+    buttons = [
+      {
+        type: 'postback',
+        title: 'Synopsis',
+        payload: 'get_synopsis'
+      }
+    ];
+    /* HARD CODED */
+    next_part = 1;
+  } else {
+    // Grab next entry
+    text = nextSummary.summary;
+    next_part = current_part + 1;
+    buttons = [
+      {
+        type: 'postback',
+        title: 'Continue',
+        payload: 'get_summary'
+      }
+    ];
+  }
+
+  await ChatReads.edit(
+    { user_id: 1, book_id: 1 },
+    { current_summary_id: next_part }
+  );
+
+  return {
+    attachment: {
+      type: 'template',
+      payload: {
+        template_type: 'button',
+        text: text,
+        buttons
+      }
+    }
+  };
+};
