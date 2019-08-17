9f9df703b5723ab19c30bd6520f34a24ac0fd01e
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-16 21:38:01 -0400

    removes hard-coding to allow more than one FB user

--- routes/messages/commands/get_started.js
+++ routes/messages/commands/get_started.js
@@ -1,37 +1,42 @@
-const Command = require('../classes/Command.js');
 const ChatReads = require('../../../models/db/chatReads.js');
+const Books = require('../../../models/db/books.js');
+const Users = require('../../../models/db/users.js');
+
+// Verify users exists already, if not save their Facebook ID
+// Short term: reset current_summary of the book, fetch book from DB, display get Synopsis option
+// Long term: Suggest books / categories for user to select
 
 module.exports = async event => {
-  try {
-    /* HARD CODED */
-    await ChatReads.write(
-      { user_id: 1, book_id: 1 },
-      { current_summary_id: 1 }
-    );
-    const response = {
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'generic',
-          elements: [
-            {
-              title: 'Shoe Dog',
-              subtitle: 'by Phil Knight',
-              buttons: [
-                {
-                  type: 'postback',
-                  title: 'Synopsis',
-                  payload: 'get_synopsis'
-                }
-              ]
-            }
-          ]
-        }
-      }
-    };
+  let user = await Users.retrieveOrCreate({ facebook_id: event.sender.id });
 
-    new Command(response, event).sendResponse();
-  } catch (err) {
-    console.log('get_started error: ', err);
-  }
+  /* HARD CODED */
+  const book_id = 1;
+  const book = await Books.retrieve({ id: book_id }).first();
+
+  await ChatReads.editOrCreate(
+    { user_id: user.id, book_id },
+    { current_summary_id: 1 }
+  );
+
+  return {
+    attachment: {
+      type: 'template',
+      payload: {
+        template_type: 'generic',
+        elements: [
+          {
+            title: book.title,
+            subtitle: `by ${book.author}`,
+            buttons: [
+              {
+                type: 'postback',
+                title: 'Synopsis',
+                payload: 'get_synopsis'
+              }
+            ]
+          }
+        ]
+      }
+    }
+  };
 };
