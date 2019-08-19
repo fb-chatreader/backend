9aabe55d463efbae714f4dadc24d1628ba1e1d59
Vance Leon <vanceleon44@gmail.com>
2019-08-19 18:01:43 -0400

    amazon payload in prog deleted postbacks folder and added backup code file

---
+++ routes/messages/commands/amazon_link.js
@@ -0,0 +1,17 @@
+// const Command = require('../classes/Command.js');
+// const Books = require('../../../models/db/books.js');
+
+
+// module.exports = async event => {
+//     const id = 1; //need a dynamic way to pull ids  
+//     const id = await Books.retrieve({id}).first();
+//     return {
+//         attachments: {
+//             type: 'template',
+//             payload: {
+//                 template_type: 'button',
+//                 text: book.
+//             }
+//         }
+//     }
+// }
