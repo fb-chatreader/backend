e21f9e87861b8a599233d06b3e49fcde63ced109
Vance Leon <vanceleon44@gmail.com>
2019-08-19 19:50:25 -0400

    built out event for amazon purchase link

--- routes/messages/commands/amazon_link.js
+++ routes/messages/commands/amazon_link.js
@@ -1,17 +1,36 @@
-// const Command = require('../classes/Command.js');
-// const Books = require('../../../models/db/books.js');
+const Command = require('../classes/Command.js');
+const Books = require('../../../models/db/books.js');
 
-
-// module.exports = async event => {
-//     const id = 1; //need a dynamic way to pull ids  
-//     const id = await Books.retrieve({id}).first();
-//     return {
-//         attachments: {
-//             type: 'template',
-//             payload: {
-//                 template_type: 'button',
-//                 text: book.
-//             }
-//         }
-//     }
-// }
+module.exports = async event => {
+  const id = 1; //need a dynamic way to pull ids
+  const book = await Books.retrieve({ id }).first();
+  return {
+    attachments: {
+      type: 'template',
+      payload: {
+        template_type: 'generic',
+        elements: [
+          {
+            title: book.title,
+            image_url: book.image_url,
+            subtitle: 'Purchase on Amazon',
+            default_action: {
+              type: 'web_url',
+              url: 'https://www.amazon.com/Shoe-Dog-Phil-Knight/dp/1508211809',
+              messenger_extensions: FALSE,
+              webview_height_ration: 'FULL'
+            },
+            buttons: [
+              {
+                type: 'web_url',
+                url:
+                  'https://www.amazon.com/Shoe-Dog-Phil-Knight/dp/1508211809',
+                title: 'Buy on Amazon'
+              }
+            ]
+          }
+        ]
+      }
+    }
+  };
+};
