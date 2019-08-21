57e8b16a783b5e625af8fcfe35542ab00b203392
Vance Leon <vanceleon44@gmail.com>
2019-08-21 18:43:39 -0400

    amazon link fully functional

--- routes/messages/commands/amazon_link.js
+++ routes/messages/commands/amazon_link.js
@@ -6,45 +6,44 @@ const getUserInfo = require('../util/asycFunctions');
 module.exports = async event => {
   const id = 1; //need a dynamic way to pull ids
   const book = await Books.retrieve({ id }).first();
-  // const user = await Users.retrieveOrCreate({ facebook_id: event.sender.id });
-
-  // await ChatReads.editOrCreate(
-  //   { user_id: user.id, book_id },
-  //   { current_summary_id: 1 }
-  // );
 
   const user_info = await getUserInfo(event.sender.id);
 
   return [
     {
-      text: `${user_info.first_name}, thank you for reading a quick summary of ${book.title}, I hope you liked it! You can buy a copy of the book here:`
+      text: `${
+        user_info.first_name
+      }, thank you for reading a quick summary of ${
+        book.title
+      }, I hope you liked it! You can buy a copy of the book here:`
     },
     {
-    attachment: {
-      type: 'template',
-      payload: {
-        template_type: 'generic',
-        elements: [
-          {
-            title: book.title,
-            image_url: book.cover_img,
-            default_action: {
-              type: 'web_url',
-              url:
-                'https://www.amazon.com/Shoe-Dog-Memoir-Creator-Nike/dp/1501135929',
-              webview_height_ratio: 'FULL'
-            },
-            buttons: [
-              {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'generic',
+          elements: [
+            {
+              title: book.title,
+              image_url: book.cover_img,
+              default_action: {
                 type: 'web_url',
                 url:
                   'https://www.amazon.com/Shoe-Dog-Memoir-Creator-Nike/dp/1501135929',
-                title: 'Buy on Amazon'
-              }
-            ]
-          }
-        ]
+                webview_height_ratio: 'FULL'
+              },
+              buttons: [
+                {
+                  type: 'web_url',
+                  url:
+                    'https://www.amazon.com/Shoe-Dog-Memoir-Creator-Nike/dp/1501135929',
+                  title: 'Buy on Amazon'
+                }
+              ]
+            }
+          ]
+        }
       }
     }
-  }];
+  ];
 };
