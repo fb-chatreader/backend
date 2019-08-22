6b3e5a96d06980bbd1438afd9aeea83d10c1614d
Vance Leon <vanceleon44@gmail.com>
2019-08-22 14:10:45 -0400

    editing commandlist for a persistent Menu adding a persistent menu response to messenger

---
+++ routes/messages/commands/persistent_menu.js
@@ -0,0 +1,29 @@
+const Command = require('../classes/Command.js');
+
+module.exports = async event => {
+  const persistent_menu = [
+    {
+      locale: 'default',
+      composer_input_disabled: false,
+      call_to_actions: [
+        {
+          type: 'postback',
+          title: 'Talk to an agent',
+          payload: 'CARE_HELP'
+        },
+        {
+          type: 'postback',
+          title: 'Outfit suggestions',
+          payload: 'CURATION'
+        },
+        {
+          type: 'web_url',
+          title: 'Shop now',
+          url: 'https://www.originalcoastclothing.com/',
+          webview_height_ratio: 'full'
+        }
+      ]
+    }
+  ];
+  new Command(persistent_menu, event).sendResponse();
+};
