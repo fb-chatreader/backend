6ed48f6f8ae24ccd6c855ae092a083672a024df9
A.D.Faris <adfaris@gmail.com>
2019-08-22 17:56:11 -0400

    image quality good both desktop and phone

--- models/seeds/02-books.js
+++ models/seeds/02-books.js
@@ -12,8 +12,7 @@ exports.seed = function(knex, Promise) {
           author: 'Phil Knight',
           synopsis:
             'Shoe Dog (2016) tells the story of the man behind the famous footwear company Nike. These blinks offer a peek into the mind of genius entrepreneur Phil Knight and detail the rollercoaster ride he went to through to build up his company.',
-          cover_img:
-            'https://cdn1.imggmi.com/uploads/2019/8/22/a3cd260d27f5d97ff5b1380f283e6cc2-full.jpg',
+          cover_img:'https://cdn1.imggmi.com/uploads/2019/8/23/80695b1acdb40ed02bb8d1a310dd9941-full.jpg',
           publish_date: 'April 26, 2016',
           intro:
             "I'm Phil Knight and I'm the founding CEO of Nike, wanted to share with you a quick preview of my book Shoe Dog",
