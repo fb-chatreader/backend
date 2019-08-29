e8e2507850de315e4dd7a1e30e24ec329c311687
Vance Leon <vanceleon44@gmail.com>
2019-08-29 14:59:28 -0400

    users_categories table

--- models/migrations/20190829130000_user_categories.js
+++ models/migrations/20190829130000_user_categories.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('users_categories', tbl => {
+    tbl.increments();
+    tbl
+      .foreign('to_categories')
+      .references('categories_id')
+      .inTable('categories');
+    tbl
+      .foreign('to_users')
+      .references('users_id')
+      .inTable('users');
+    tbl.timestamp('created_at', { useTz: true });
+  });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('users_categories');
 };
