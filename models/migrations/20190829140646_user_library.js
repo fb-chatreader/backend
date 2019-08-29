fc705ca07d01355b6c0d65363b7986c5415a1f32
Vance Leon <vanceleon44@gmail.com>
2019-08-29 17:50:16 -0400

    user_library table'

--- models/migrations/20190829140646_user_library.js
+++ models/migrations/20190829140646_user_library.js
@@ -1,11 +1,23 @@
-
 exports.up = function(knex) {
-  return knex.schema.createTable('users_library', tbl => {
-      tbl.increments();
-       
-  })
+  return knex.schema.createTable('library', tbl => {
+    tbl.increments();
+    tbl
+      .foreign('users_to_library')
+      .reference('user_id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+      .notNullable();
+    tbl
+      .foreign('library_to_books')
+      .reference('books_id')
+      .inTable('books')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+      .notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('library');
 };
