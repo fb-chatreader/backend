658cb965801b9e48eb1b80799a1aeac004029a4a
Vance Leon <vanceleon44@gmail.com>
2019-08-29 19:14:49 -0400

    reverted initial table setup back to master branch

---
+++ models/migrations/20190829190958_users_table_update.js
@@ -0,0 +1,41 @@
+
+exports.up = function(knex) {
+  
+};
+
+exports.down = function(knex) {
+  
+};
+
+
+
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', tbl => {
+      tbl.increments();
+      tbl.text('facebook_id');
+      tbl.text('email');
+      tbl
+        .foreign('users_categories_id')
+        .references('categories_id')
+        .inTable('user_categories')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+        .notNullable();
+  
+      tbl
+        .foreign('users_to_library')
+        .references('users_library_id')
+        .inTable('users_library')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+        .notNullable();
+  
+      tbl.timestamp('created_at', { useTz: true });
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+  };
+  
