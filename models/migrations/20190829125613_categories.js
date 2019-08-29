0ae1973918ae209851fbe7d9ddc2c6e58ee0ed7c
Vance Leon <vanceleon44@gmail.com>
2019-08-29 14:50:53 -0400

    adding categories to table

--- models/migrations/20190829125613_categories.js
+++ models/migrations/20190829125613_categories.js
@@ -1,10 +1,54 @@
-
 exports.up = function(knex) {
-    return knex.schema.createTable('categories', tbl => {
-        
-    })
+  return knex.schema.createTable('categories', tbl => {
+    tbl.increments();
+    tbl.text('architecture');
+    tbl.text('art');
+    tbl.text('biography');
+    tbl.text('body_mind_Spirit');
+    tbl.text('computers');
+    tbl.text('cooking');
+    tbl.timestamp('created_at', { useTz: true });
+  });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('categories')
+  return knex.schema.dropTable('categories');
 };
+
+// other categories for later
+// crafts_hobbies varchar
+// drama varchar
+// education varchar
+// family_relatioships varchar
+// foreign_languages varchar
+// games varchar
+// gardening varchar
+// health varchar
+// history varchar
+// house varchar
+// humor varchar
+// languages varchar
+// law varchar
+// literary varchar
+// math varchar
+// medical varchar
+// music varchar
+// nature varchar
+// performing_arts varchar
+// pets varchar
+// philosophy varchar
+// photography varchar
+// poetry varchar
+// political_science varchar
+// psychology varchar
+// reference varchar
+// religion varchar
+// science varchar
+// self_help varchar
+// social_science varchar
+// sports varchar
+// technology varchar
+// transportation varchar
+// travel varchar
+// true_crime varchar
+// other varchar
