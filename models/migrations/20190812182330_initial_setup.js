6c862c4e3b0a67d5d056814f8ea2a69b902497ff
Vance Leon <vanceleon44@gmail.com>
2019-08-29 15:07:04 -0400

    user categories update user table reference to user categories

--- models/migrations/20190812182330_initial_setup.js
+++ models/migrations/20190812182330_initial_setup.js
@@ -3,9 +3,15 @@ exports.up = function(knex) {
     tbl.increments();
     tbl.text('facebook_id');
     tbl.text('email');
-    tbl.text('users_categories-id');
-    tbl.foreign('library').references('users-libraryId').inTable('userLibrary');
-    
+    tbl
+      .foreign('users_categories_id')
+      .references('categories_id')
+      .inTable('user_categories');
+    tbl
+      .foreign('library')
+      .references('users-libraryId')
+      .inTable('userLibrary');
+
     tbl.timestamp('created_at', { useTz: true });
   });
 };
