e1027984f205e5c96eab3ad84bd308fc8b882667
Vance Leon <vanceleon44@gmail.com>
2019-08-29 20:16:19 -0400

    changed int to integer

--- models/migrations/20190829130000_user_categories.js
+++ models/migrations/20190829130000_user_categories.js
@@ -2,11 +2,11 @@ exports.up = function(knex) {
   return knex.schema.createTable('users_categories', tbl => {
     tbl.increments();
     tbl
-      .int('to_categories')
+      .integer('to_categories')
       .references('categories_id')
       .inTable('categories');
     tbl
-      .int('to_users')
+      .integer('to_users')
       .references('users_id')
       .inTable('users');
     tbl.timestamp('created_at', { useTz: true });
