39c6fa110a60f576917e46fbb5baf91936c62bd2
Vance Leon <vanceleon44@gmail.com>
2019-08-29 20:42:54 -0400

    book_categories

--- models/migrations/20190829203254_book_categories.js
+++ models/migrations/20190829203254_book_categories.js
@@ -1,15 +1,17 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('user_libraries', tbl => {
+  return knex.schema.createTable('book_categories', tbl => {
     tbl.increments();
-    tbl
-      .integer('user_id')
-      .references('id')
-      .inTable('users')
-
     tbl
       .integer('book_id')
       .references('id')
       .inTable('books')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+      .notNullable();
+    tbl
+      .integer('category_id')
+      .references('id')
+      .inTable('categories')
       .onDelete('CASCADE')
       .onUpdate('CASCADE')
       .notNullable();
