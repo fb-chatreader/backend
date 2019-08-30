0af79fe9fa3307259e265111cbf40a282bb28d68
Vance Leon <vanceleon44@gmail.com>
2019-08-30 13:07:12 -0400

    writing categories to the db

--- routes/books/booksRoutes.js
+++ routes/books/booksRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Books = require('../../models/db/books');
+const Categories = require('../../models/db/categories');
 
 router.post('/add', async (req, res) => {
   const books = req.body; //rec content
@@ -11,8 +12,9 @@ router.post('/add', async (req, res) => {
     book.created_at = created_at;
 
     await Books.write(book);
-
-    categories
+    // categories[category]
+    const { category } = categories.retrieve({ [category]: 1 }).first();
+    await Categories.write(category);
   }
 });
 
