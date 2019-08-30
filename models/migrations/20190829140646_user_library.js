ca2a1b548c7888debdc15dd49c9bcce37f682b09
Vance Leon <vanceleon44@gmail.com>
2019-08-29 20:10:35 -0400

    users alter table fixed with just email added on now books alter has dropcolumn

--- models/migrations/20190829140646_user_library.js
+++ models/migrations/20190829140646_user_library.js
@@ -1,13 +1,26 @@
 exports.up = function(knex) {
-    return knex.schema.alterTable('books', tbl => {
-      tbl
-        // .foreign('books_to_categories')
-        .references('categories_id')
-        .inTable('categories')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE')
-        .notNullable();
-
+  return knex.schema.alterTable('books', tbl => {
+    tbl.increments();
+    tbl
+      .int('users_to_library')
+      .reference('user_id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+      .notNullable();
+    tbl
+      .int('library_to_books')
+      .reference('books_id')
+      .inTable('books')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+      .notNullable();
   });
 };
 
+exports.down = function(knex) {
+  return knex.schema.alterTable('books', tbl => {
+    tbl.dropColumn('users_to_library');
+    tbl.dropColumn('library_to_books');
+  });
+};
