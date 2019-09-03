e188b9066d405c315e7fa1d08b289fedd81a784b
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-03 17:29:08 -0400

    Renames cover-Img to match FBAPI language

--- models/seeds/02-books.js
+++ models/seeds/02-books.js
@@ -27,7 +27,7 @@ exports.seed = function(knex) {
     author: 'Phil Knight',
     synopsis:
       'Shoe Dog (2016) tells the story of the man behind the famous footwear company Nike. These blinks offer a peek into the mind of genius entrepreneur Phil Knight and detail the rollercoaster ride he went to through to build up his company.',
-    cover_img:
+    image_url:
       'https://cdn1.imggmi.com/uploads/2019/8/23/80695b1acdb40ed02bb8d1a310dd9941-full.jpg',
     intro:
       "I'm Phil Knight and I'm the founding CEO of Nike, wanted to share with you a quick preview of my book Shoe Dog"
