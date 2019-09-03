cba2965bc473d994029030109baa0293ee86d594
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-03 16:39:39 -0400

    Alters check for saving PSID to occur at middleware instead of 'get_started'

--- middleware/webhooks.js
+++ middleware/webhooks.js
@@ -1,28 +1,49 @@
+const Users = require('models/db/users.js');
+
 const verifyWebhook = ({ body }, res, next) => {
   if (body.object === 'page') {
     next();
   }
 };
 
-const formatWebhook = ({ body: { entry } }, res, next) => {
+const formatWebhook = async ({ body: { entry } }, res, next) => {
   // We receive data for our commands from a variety of places.
   // This middleware is meant to organize that data into a single place to
   // simplify the rest of our code
-  let parsed_data;
+
   const event =
     entry && entry[0] && entry[0].messaging ? entry[0].messaging[0] : null;
+  if (event) {
+    // Save sender ID in DB if it doesn't already exist
+    const exists = await Users.retrieve({ facebook_id: event.sender.id });
+    if (!exists) {
+      await Users.add({ facebook_id: event.sender.id });
+    }
+  }
 
+  if (isValidMessengerRequest(entry, event)) {
+    entry[0].input = formatEventObject(entry, event);
+    next();
+  } else {
+    return res.sendStatus(400);
+  }
+};
+
+module.exports = { verifyWebhook, formatWebhook };
+
+function formatEventObject(entry, event) {
   // Order of importance for webhooks --> Policy violations > Postback > Commands
   // Type added in case we need to verify source (do we want users to say "policy violation"
   // and trigger our policy violation command?)
-  if (entry && entry[0] && entry[0]['policy-enforcement']) {
+  let parsed_data;
+  if (entry[0]['policy-enforcement']) {
     parsed_data = {
       command: 'policy_violation',
       type: 'policy_violation',
       ...entry[0]['policy-enforcement'],
       page_id: entry[0].recipient.id
     };
-  } else if (event && event.postback) {
+  } else if (event.postback) {
     parsed_data = {
       ...JSON.parse(event.postback.payload),
       type: 'postback',
@@ -38,12 +59,9 @@ const formatWebhook = ({ body: { entry } }, res, next) => {
       sender: event.sender
     };
   }
-  if (entry && entry[0] && parsed_data) {
-    entry[0].input = parsed_data;
-    next();
-  } else {
-    return res.sendStatus(400);
-  }
-};
+  return parsed_data;
+}
 
-module.exports = { verifyWebhook, formatWebhook };
+function isValidMessengerRequest(entry, event) {
+  return event || (entry && entry[0]);
+}
