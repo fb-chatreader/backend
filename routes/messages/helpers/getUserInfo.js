d3cd937ba269cc3ea83128a48ee54111a1b127b1
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-03 13:58:23 -0400

    Removes diagnostic log

--- routes/messages/helpers/getUserInfo.js
+++ routes/messages/helpers/getUserInfo.js
@@ -3,6 +3,5 @@ const axios = require('axios');
 module.exports = async PSID => {
   const url = `https://graph.facebook.com/${PSID}?fields=first_name&access_token=${process.env.PAGE_ACCESS_TOKEN}`;
   const request = await axios.get(url);
-  // console.log("firing get info", request);
   return await request.data;
 };
