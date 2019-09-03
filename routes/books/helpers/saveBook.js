b2d30026c5dee37984566b56dd54b06ff0e5bd3c
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-03 15:00:09 -0400

    Removes unused routes

--- routes/books/helpers/saveBook.js
+++ routes/books/helpers/saveBook.js
@@ -5,26 +5,33 @@ const Summary_Parts = require('models/db/summaryParts');
 
 module.exports = async bookObj => {
   const { summary, category, ...book } = bookObj;
-  if (!summary || !category || !book.title || !book.author || !book.cover_img) {
+  if (
+    !summary ||
+    !category ||
+    !book ||
+    !book.title ||
+    !book.author ||
+    !book.cover_img
+  ) {
     // If we're missing any part of a book, skip over it
     return;
   }
-  const newBook = await Books.write(book);
+  const newBook = await Books.add(book);
   const book_category = await Categories.retrieve({
     [category.toLowerCase()]: 1
   }).first();
-  await Book_Categories.write({
-    book_id: newBook[0].id,
+  await Book_Categories.add({
+    book_id: newBook.id,
     category_id: book_category.id
   });
 
   const summaryArray = getSummaryParts(summary);
   for (let i = 0; i < summaryArray.length; i++) {
     const summaryObj = {
-      book_id: newBook[0].id,
+      book_id: newBook.id,
       summary: summaryArray[i]
     };
-    await Summary_Parts.write(summaryObj);
+    await Summary_Parts.add(summaryObj);
   }
   return;
 };
