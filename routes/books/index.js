286fe0e30592d041c59587969422ec96d7c6dfa2
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-03 16:24:12 -0400

    Removes diag logs

--- routes/books/index.js
+++ routes/books/index.js
@@ -6,10 +6,8 @@ router.post('/add', async (req, res) => {
   console.log('Receiving books');
   const books = req.body;
   if (Array.isArray(books)) {
-    console.log('Input is array: ', books.length);
     // If endpoint receives an array of books
     for (let i = 0; i < books.length; i++) {
-      console.log('Saving (for) Book: ', books[i]);
       const save = await saveBook(books[i]);
       if (!save) {
         return res.status(400).json({
@@ -21,7 +19,6 @@ router.post('/add', async (req, res) => {
     return res.sendStatus(201);
   } else {
     // if Endpoint receives a single book object
-    console.log('Saving (single) Book: ', books);
     const save = await saveBook(books);
     return save
       ? res.sendStatus(201)
