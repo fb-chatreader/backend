89e8bf0dfb30e824554f990ed09e2bc181cf7ca1
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-03 15:43:11 -0400

    Fixes bugs added by refactor

--- routes/books/index.js
+++ routes/books/index.js
@@ -1,21 +1,34 @@
 const router = require('express').Router();
 
 const saveBook = require('./helpers/saveBook.js');
-const getSummaryParts = require('./helpers/getSummaryParts.js');
 
 router.post('/add', async (req, res) => {
+  console.log('Receiving books');
   const books = req.body;
-
   if (Array.isArray(books)) {
+    console.log('Input is array: ', books.length);
     // If endpoint receives an array of books
     for (let i = 0; i < books.length; i++) {
-      await saveBook(books[i]);
-      return res.sendStatus(201);
+      console.log('Saving (for) Book: ', books[i]);
+      const save = await saveBook(books[i]);
+      if (!save) {
+        return res.status(400).json({
+          message:
+            'Books must include a summary, category, title, author, and cover image'
+        });
+      }
     }
+    return res.sendStatus(201);
   } else {
     // if Endpoint receives a single book object
-    await saveBook(books);
-    return res.sendStatus(201);
+    console.log('Saving (single) Book: ', books);
+    const save = await saveBook(books);
+    return save
+      ? res.sendStatus(201)
+      : res.status(400).json({
+          message:
+            'Books must include a summary, category, title, author, and cover image'
+        });
   }
 });
 
