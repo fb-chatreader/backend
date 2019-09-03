606f70b11375bf9161e89c71750a5c033d728caa
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-03 13:21:38 -0400

    Adds default '404' route

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 // Jobs -- Tasks running on a schedule
 require('./jobs/timedMessages.js');
 
-// routes
+// Import routes
 const messageRouter = require('./routes/messages/');
 const bookRouter = require('./routes/books/');
 const categoryRouter = require('./routes/categories/');
@@ -13,17 +13,22 @@ const categoryRouter = require('./routes/categories/');
 // Error handling
 const errorHandler = require('./middleware/errorHandling');
 
+// Server config
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-// routes
+// Use routes
 server.use('/api/messenger', messageRouter);
 server.use('/api/books', bookRouter);
 server.use('/api/category', categoryRouter);
 
 server.get('/', (req, res) => {
   return res.send(200).json({ message: 'Welcome to ChatReader server!' });
+});
+
+server.get('*', function(req, res) {
+  res.status(404).send('Sorry, that is not a valid route');
 });
 
 //async error handling middleware MUST come after routes or else will just throw Type error
