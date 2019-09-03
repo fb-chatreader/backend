bb3857a901a5d9e9eed810a1a21812f23f9b858b
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-03 14:02:25 -0400

    Further cleans up commands

--- routes/messages/commands/policy_violation.js
+++ routes/messages/commands/policy_violation.js
@@ -3,7 +3,7 @@ const Violations = require('models/db/violations.js');
 module.exports = async event => {
   const { action, reason, page_id } = event;
   if (event.type === 'policy_violation') {
-    await Violations.write({ page_id, action, reason, created_at: new Date() });
+    await Violations.write({ page_id, action, reason });
   }
   return null;
 };
