f6b0a95f226dce7f1b12ffdc3ed6d89de1052e44
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-04 19:55:20 -0400

    Adds req param to get client ID

--- routes/messages/index.js
+++ routes/messages/index.js
@@ -3,25 +3,33 @@ const router = require('express')
   .Router()
   .use(bodyParser.json());
 
+const Client = require('models/db/clients.js');
+
 const { verifyWebhook, formatWebhook } = require('middleware/webhooks.js');
 
 const CommandListClass = require('classes/CommandList.js');
 const CommandList = new CommandListClass();
 
-router.post('/webhook', verifyWebhook, formatWebhook, (req, res) => {
+router.post('/:client_id', verifyWebhook, formatWebhook, (req, res) => {
   const input = req.body.entry[0].input;
   const sent = CommandList.execute(input);
   return sent ? res.sendStatus(200) : res.sendStatus(404);
 });
 
-router.get('/webhook', (req, res) => {
-  let VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+router.get('/:client_id', async (req, res) => {
+  const { client_id } = req.params;
+  const client = await Client.retrieve({ id: client_id }).first();
+
+  if (!client) return res.sendStatus(404);
+
+  const { verification_token } = client;
+
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
 
   if (mode && token) {
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    if (mode === 'subscribe' && token === verification_token) {
       console.log('Webhook verified');
       return res.status(200).send(challenge);
     } else {
