9468fb02566d163aa53e6086c548aec7e8cebb0b
Vance Leon <vanceleon44@gmail.com>
2019-09-04 14:27:02 -0400

    test list up

--- routes/messages/commands/list-temp.js
+++ routes/messages/commands/list-temp.js
@@ -3,59 +3,58 @@ const ChatReads = require('../../../models/db/chatReads.js');
 const Books = require('../../../models/db/books.js');
 
 module.exports = async event => {
-
-    const books = await Books.retrieve();
-    "payload" = {
-        "template_type": "list",
-        "top_element_style": "<LARGE | COMPACT>",
-        "elements": [
+  const books = await Books.retrieve();
+  if (!books.length) return;
+  return {
+    template_type: 'list',
+    top_element_style: 'LARGE',
+    elements: [
+      {
+        title: 'Testing List',
+        subtitle: 'List Test',
+        image_url: books[0].cover_img,
+        buttons: [
           {
-            "title": "Testing List",
-            "subtitle": "List Test",
-            "image_url": books[0].cover_img,          
-            "buttons": [
-                {
-                  type: 'postback',
-                  title: 'Start Summary',
-                  payload: JSON.stringify({
-                    command: 'get_summary',
-                    book_id: id
-                  })
-                }
-              ],
-            "default_action": {
-              "type": "web_url",
-              "messenger_extensions": FALSE,
-              "webview_height_ratio": "TALL "
-            }
-          },
-          
+            type: 'postback',
+            title: 'Start Summary',
+            payload: JSON.stringify({
+              command: 'get_summary',
+              book_id: id
+            })
+          }
         ],
-         "buttons": [
-            {
-              type: 'postback',
-              title: 'Start Summary',
-              payload: JSON.stringify({
-                command: 'get_summary',
-                book_id: id
-              })
-            },
-            {
-              type: 'postback',
-              title: 'Start Summary',
-              payload: JSON.stringify({
-                command: 'get_summary',
-                book_id: id
-              })
-            },
-            {
-              type: 'postback',
-              title: 'Start Summary',
-              payload: JSON.stringify({
-                command: 'get_summary',
-                book_id: id
-              })
-            },
-          ]
+        default_action: {
+          type: 'web_url',
+          messenger_extensions: FALSE,
+          webview_height_ratio: 'TALL '
+        }
       }
-}
+    ],
+    buttons: [
+      {
+        type: 'postback',
+        title: 'Start Summary',
+        payload: JSON.stringify({
+          command: 'get_summary',
+          book_id: id
+        })
+      },
+      {
+        type: 'postback',
+        title: 'Start Summary',
+        payload: JSON.stringify({
+          command: 'get_summary',
+          book_id: id
+        })
+      },
+      {
+        type: 'postback',
+        title: 'Start Summary',
+        payload: JSON.stringify({
+          command: 'get_summary',
+          book_id: id
+        })
+      }
+    ]
+  };
+};
