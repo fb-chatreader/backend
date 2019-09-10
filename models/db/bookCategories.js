c781d13de5af0cd4ae547b1ec1573c03dec71df9
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-10 19:45:51 -0400

    Resolves issues from switching categories to a single column

--- models/db/bookCategories.js
+++ models/db/bookCategories.js
@@ -6,11 +6,32 @@ module.exports = {
 };
 
 function retrieve(filter) {
-  return filter ? db('book_categories').where(filter) : db('book_categories');
+  return filter
+    ? db('book_categories as bc')
+        .select(
+          'b.id as id',
+          'b.page_id as page_id',
+          'b.title as title',
+          'b.author as author',
+          'b.intro as intro',
+          'b.synopsis as synopsis',
+          'b.image_url as image_url'
+        )
+        .join('books as b', { 'b.id': 'bc.book_id' })
+        .where(filter)
+    : db('book_categories as bc')
+        .select(
+          'bc.book_id as id',
+          'b.page_id as page_id',
+          'b.author as author',
+          'b.intro as intro',
+          'b.image_url as image_url'
+        )
+        .join('books as b', { 'b.id': 'bc.book_id' });
 }
 
 function add(category) {
   return db('book_categories')
     .insert(category, ['*'])
-    .then(c => retrieve({ id: c[0].id }).first());
+    .then(c => retrieve({ 'bc.id': c[0].id }).first());
 }
