b26e265af61dc4ae59ca227b1a307fb02e05b2e2
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-10 19:46:24 -0400

    Fixes bugs when saving user email

--- routes/messages/commands/save_email.js
+++ routes/messages/commands/save_email.js
@@ -4,8 +4,8 @@ module.exports = async event => {
   // Currently just saves an email when presented with one.
   // Future work:
   // Return a series (3) of carousels based on the user's preferred categories, if they exist.
-  const { user } = event;
-  await Users.edit(user.id, (email = event.original_message));
+  const { user_id: id } = event;
+  await Users.edit({ id }, { email: event.original_message });
 
   return pickCategory(event);
 };
