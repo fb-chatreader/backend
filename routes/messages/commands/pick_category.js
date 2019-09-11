5a0ac99d65c6142400a39392f9f8b4e4c594072c
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-11 17:54:54 -0400

    Now ignores users while they're selecting their favorite categories

--- routes/messages/commands/pick_category.js
+++ routes/messages/commands/pick_category.js
@@ -3,9 +3,11 @@ const Books = require('models/db/books.js');
 const BookCategories = require('models/db/bookCategories.js');
 const UserCategories = require('models/db/userCategories.js');
 
-const { getNewCategoriesForUser } = require('../helpers/categories.js');
+// const { getNewCategoriesForUser } = require('../helpers/categories.js');
 
 module.exports = async event => {
+  await event;
+
   if (
     event.type !== 'postback' &&
     event.command !== 'get_started' &&
@@ -28,118 +30,101 @@ module.exports = async event => {
 
   if (!userCategoryIDs.length) return;
 
-  return userCategoryIDs.length < 3
-    ? getNextFavorite(userCategoryIDs, user_id)
-    : finishCategories(userCategoryIDs, event);
+  if (userCategoryIDs.length >= 3)
+    return event.user.email
+      ? finishCategories(userCategoryIDs, event)
+      : getEmail(event);
 };
 
-async function getNextFavorite(userCategoryIDs, user_id) {
-  const remainingCategories = await getNewCategoriesForUser(user_id);
+async function getEmail(event) {
+  const textResponses = {
+    pick_category:
+      "Great, I have what I need to make some suggestions!  Though first, I'd like to make an account for you so I can remember your preferences across platforms.  What email address can I attach to your account?",
+    get_started:
+      'I have some suggestions ready to go for you, just respond with a valid email and I can get them right to you!',
+    default:
+      "We use an email address to identify you across platforms.  Type a valid email address at any time and I'll save it to your account so you can proceed!"
+  };
 
-  const text =
-    userCategoryIDs.length === 1
-      ? 'Thanks, now pick a second favorite:'
-      : 'Great!  Okay, last one:';
+  const text = textResponses[event.command]
+    ? textResponses[event.command]
+    : textResponses.default;
 
   return [
     {
-      attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'button',
-          text,
-          buttons: remainingCategories.map(c => {
-            // Everything except the category name must be destructured
-            // for this to work
-            const { id, name: title } = c;
-
-            return {
-              type: 'postback',
-              title: title[0].toUpperCase() + title.substring(1),
-              payload: JSON.stringify({
-                command: 'pick_category',
-                category_id: id
-              })
-            };
-          })
-        }
-      }
+      text
     }
   ];
 }
 
 async function finishCategories(userCategoryIDs, event) {
   const {
-    user_id,
     page: { id: page_id }
   } = event;
-  const user = await Users.retrieve({ id: user_id }).first();
 
-  const text = user.email
-    ? 'So based on your preferences, here are some books you might like!'
-    : "Great, I have what I need to make some suggestions!  Though first, I'd like to make an account for you so I can remember them across platforms.  What email address can I attach to your account?";
+  const text = 'Based on your preferences, here are some books you might like!';
+
   // Convert IDs into names
   //   const rawCategories = await Promise.all(
   //     userCategoryIDs.map(id => Categories.retrieve({ id }).first())
   //   );
   //   const categories = cleanCategories(rawCategories);
-  const carousels = user.email
-    ? await Promise.all(
-        userCategoryIDs.map(async category_id => {
-          const pageBooks = await BookCategories.retrieve({
-            'bc.category_id': category_id,
-            page_id
-          });
+  const carousels = await Promise.all(
+    userCategoryIDs.map(async category_id => {
+      const pageBooks = await BookCategories.retrieve({
+        'bc.category_id': category_id,
+        page_id
+      });
 
-          return pageBooks.length
-            ? {
-                attachment: {
-                  type: 'template',
-                  payload: {
-                    template_type: 'generic',
-                    elements: pageBooks.map(b => {
-                      const buttons = [];
-                      if (b.synopsis) {
-                        buttons.push({
-                          type: 'postback',
-                          title: 'Read Synopsis',
-                          payload: JSON.stringify({
-                            command: 'get_synopsis',
-                            book_id: b.id
-                          })
-                        });
-                      }
-                      buttons.push({
-                        type: 'postback',
-                        title: 'Start Summary',
-                        payload: JSON.stringify({
-                          command: 'get_summary',
-                          book_id: b.id
-                        })
-                      });
-
-                      buttons.push({
-                        type: 'postback',
-                        title: 'Save to Library',
-                        payload: JSON.stringify({
-                          command: 'save_to_library',
-                          book_id: b.id
-                        })
-                      });
-
-                      return {
-                        title: b.title,
-                        image_url: b.image_url,
-                        subtitle: `by ${b.author}`,
-                        buttons
-                      };
-                    })
+      return pageBooks.length
+        ? {
+            attachment: {
+              type: 'template',
+              payload: {
+                template_type: 'generic',
+                elements: pageBooks.map(b => {
+                  const buttons = [];
+                  if (b.synopsis) {
+                    buttons.push({
+                      type: 'postback',
+                      title: 'Read Synopsis',
+                      payload: JSON.stringify({
+                        command: 'get_synopsis',
+                        book_id: b.id
+                      })
+                    });
                   }
-                }
+                  buttons.push({
+                    type: 'postback',
+                    title: 'Start Summary',
+                    payload: JSON.stringify({
+                      command: 'get_summary',
+                      book_id: b.id
+                    })
+                  });
+
+                  buttons.push({
+                    type: 'postback',
+                    title: 'Save to Library',
+                    payload: JSON.stringify({
+                      command: 'save_to_library',
+                      book_id: b.id
+                    })
+                  });
+
+                  return {
+                    title: b.title,
+                    image_url: b.image_url,
+                    subtitle: `by ${b.author}`,
+                    buttons
+                  };
+                })
               }
-            : null;
-        })
-      )
-    : null;
+            }
+          }
+        : null;
+    })
+  );
+
   return [{ text }, ...carousels];
 }
