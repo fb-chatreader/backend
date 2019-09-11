397b04123faa88ba96483f82508d9ca4afcd9ce7
Erik Kimsey <kimseyerik@gmail.com>
2019-09-11 11:09:47 -0400

    Updated env

--- models/seeds/02-users.js
+++ models/seeds/02-users.js
@@ -1,8 +1,8 @@
 exports.seed = function(knex) {
-  // delete all existing entries
-  return knex('users').insert([
-    {
-      facebook_id: 10152368852405295
-    }
-  ]);
+	// delete all existing entries
+	return knex('users').insert([
+		{
+			facebook_id: 111468236903301
+		}
+	]);
 };
