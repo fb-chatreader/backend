d21e56cbd22efb7c935984d06b3f621fa8ca950c
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-11 13:43:59 -0400

    Removes empty log

--- classes/Message.js
+++ classes/Message.js
@@ -15,7 +15,6 @@ module.exports = class Message {
     // 1) Be a promise  --> this.response.then is truthy
     // 2) Not be a promise --> this.response.then is falsey
     // 3) Contain an array of promises --> this.response.then is falsey but this.response[0].then is truthy
-    console.log();
     if (this.response.then || this.response[0].then) {
       const resolved = await this._resolvePromises();
       if (!resolved) {
