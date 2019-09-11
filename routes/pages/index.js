d843c5c07b44f7a87acaa0aa7652365e34815b1a
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-11 17:02:36 -0400

    Corrects callback URL to match using page IDs instead of client ID

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -3,13 +3,13 @@ const Pages = require('models/db/pages.js');
 
 const { validatePage } = require('middleware/pages.js');
 
-router.post('/', validatePage, async (req, res) => {
+router.post('/add', validatePage, async (req, res) => {
   const { access_token, page_id: id } = req.body;
   const newPage = await Pages.add({ access_token, id });
 
   return newPage
     ? res.status(200).json({
-        callback_URL: `${process.env.APP_URL}/api/messenger/${newPage.id}`,
+        callback_URL: `${process.env.APP_URL}/api/messenger`,
         verification_token: newPage.verification_token
       })
     : res.sendStatus(418);
