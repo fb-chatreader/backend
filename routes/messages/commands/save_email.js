4db1d1f70bc5e288dad2d5a5080d73dc3f849bfc
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-11 13:42:16 -0400

    Returns null if only one book is given

--- routes/messages/commands/save_email.js
+++ routes/messages/commands/save_email.js
@@ -1,4 +1,5 @@
 const Users = require('models/db/users.js');
+const Books = require('models/db/books.js');
 const pickCategory = require('./pick_category.js');
 module.exports = async event => {
   // Currently just saves an email when presented with one.
@@ -6,6 +7,6 @@ module.exports = async event => {
   // Return a series (3) of carousels based on the user's preferred categories, if they exist.
   const { user_id: id } = event;
   await Users.edit({ id }, { email: event.original_message });
-
-  return pickCategory(event);
+  const books = Books.retrieve({ page_id: event.page.id });
+  return books.length > 1 ? pickCategory(event) : null;
 };
