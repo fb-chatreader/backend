2145f5b352ec85a0858ed1cdfb734c41f3eb91ff
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-12 16:41:44 -0400

    Now users a single command to add or remove from a user's library

--- routes/messages/commands/view_library.js
+++ routes/messages/commands/view_library.js
@@ -32,14 +32,25 @@ module.exports = async event => {
                     })
                   });
                 }
-                buttons.push({
-                  type: 'postback',
-                  title: 'Start Summary',
-                  payload: JSON.stringify({
-                    command: 'get_summary',
-                    book_id: b.id
-                  })
-                });
+                buttons.push(
+                  {
+                    type: 'postback',
+                    title: 'Start Summary',
+                    payload: JSON.stringify({
+                      command: 'get_summary',
+                      book_id: b.id
+                    })
+                  },
+                  {
+                    type: 'postback',
+                    title: 'Remove from Library',
+                    payload: JSON.stringify({
+                      command: 'toggle_in_library',
+                      book_id: b.id,
+                      isAdding: false
+                    })
+                  }
+                );
 
                 return {
                   title: b.title,
