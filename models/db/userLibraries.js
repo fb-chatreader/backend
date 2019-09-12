01acdca2afc1813263c3b061a7e12c25f2d8181d
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-12 15:25:30 -0400

    Now returns an array of user's books

--- models/db/userLibraries.js
+++ models/db/userLibraries.js
@@ -7,7 +7,30 @@ module.exports = {
 };
 
 function retrieve(filter) {
-  return filter ? db('user_libraries').where(filter) : db('user_libraries');
+  return filter
+    ? db('user_libraries as ul')
+        .select(
+          'b.id as id',
+          'b.page_id as page_id',
+          'b.title as title',
+          'b.author as author',
+          'b.intro as intro',
+          'b.synopsis as synopsis',
+          'b.image_url as image_url'
+        )
+        .where(filter)
+        .join('books as b', { 'b.id': 'ul.book_id' })
+    : db('user_libraries as ul')
+        .select(
+          'b.id as id',
+          'b.page_id as page_id',
+          'b.title as title',
+          'b.author as author',
+          'b.intro as intro',
+          'b.synopsis as synopsis',
+          'b.image_url as image_url'
+        )
+        .join('books as b', { 'b.id': 'ul.book_id' });
 }
 
 function add(category) {
