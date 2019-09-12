3338b11d1acaf59b33feddb2ad453b2e1aef4cca
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-12 16:46:45 -0400

    Adds comment to potentially confusing code

--- routes/messages/commands/toggle_in_library.js
+++ routes/messages/commands/toggle_in_library.js
@@ -13,6 +13,10 @@ module.exports = async event => {
   const currentLibrary = await UserLibrary.retrieve({ 'ul.user_id': user_id });
   const exists = currentLibrary.find(b => b.id === book_id);
 
+  // Two conditions something will happen:
+  // 1) It's a new item and the user is trying to add it
+  // 2) The user previous saved it and they want to remove it
+  // Otherwise, don't do anything (ie: if they've already saved it and they're trying to add it)
   const method =
     !exists && isAdding ? 'add' : exists && !isAdding ? 'remove' : null;
 
