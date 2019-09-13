d9a838bee080da3e123946ec65641cf3c0e330f8
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-12 20:50:02 -0400

    On multi-book pages, the bot now offers help getting started before sending user to browse books

--- routes/messages/commands/get_started.js
+++ routes/messages/commands/get_started.js
@@ -1,134 +1,124 @@
-const Books = require('models/db/books.js');
 const getUserInfo = require('../helpers/getUserInfo.js');
 const UserCategories = require('models/db/userCategories.js');
-const { getNewCategoriesForUser } = require('../helpers/categories.js');
+const UserLibraries = require('models/db/userLibraries.js');
 
-const pickCategory = require('./pick_category.js');
+module.exports = async event => {
+  const { bookCount } = event;
 
-module.exports = async (event) => {
-	const books = await Books.retrieve({ page_id: event.page.id });
-
-	if (!books.length) {
-		return [
-			{
-				text: 'Sorry, this bot is still being created, please visit us again soon!'
-			}
-		];
-	}
-	return books.length > 1 ? getMultipleBooks(event) : getSingleBook(event, books);
+  if (!bookCount) {
+    return [
+      {
+        text:
+          'Sorry, this bot is still being created, please visit us again soon!'
+      }
+    ];
+  }
+  return bookCount > 1 ? getMultipleBooks(event) : getSingleBook(event, books);
 };
 
 async function getMultipleBooks(event) {
-	// For now, the bot assumes if there are multiple books, it's on ChatReader
-	const { user_id } = event;
-	const userCategories = await UserCategories.retrieve({ user_id });
+  // For now, the bot assumes if there are multiple books, it's on ChatReader
+  const { user_id } = event;
+  const userCategories = await UserCategories.retrieve({ user_id });
+  const userLibraries = await UserLibraries.retrieve({ user_id });
 
-	if (userCategories.length >= 3) {
-		return pickCategory(event);
-	}
-	const response =
-		userCategories.length === 0
-			? [
-					{
-						text:
-							'Hi, welcome to Chat Reader!  I can read book summaries to you to help you learn more about them before you buy!'
-					}
-				]
-			: [];
+  const introText = `If you'd like to see a list of commands you can use, type "help" at any time!  For now, here are some options for you:`;
+  const firstTimeText =
+    'Hi, welcome to Chatwise!  I can read book summaries to you to help you learn more about them before you buy! You can always say "get started" to come back to this message. Also,';
 
-	const newCategories = await getNewCategoriesForUser(event.user_id);
-	const choicesLeft = 3 - userCategories.length;
-	let text = `Of the genres below, click your ${choicesLeft === 1 ? 'top' : choicesLeft} favorite${choicesLeft > 1
-		? 's'
-		: ''}!`;
+  const text =
+    userCategories.length === 0
+      ? `${firstTimeText} ${introText[0].toLowerCase()}${introText.substring(
+          1
+        )}`
+      : introText;
 
-	if (!userCategories.length) {
-		text = "Let's get started! " + text;
-	}
+  const buttons = [
+    {
+      type: 'postback',
+      title: 'Browse Books',
+      payload: JSON.stringify({
+        command: 'browse'
+      })
+    }
+  ];
 
-	response.push({
-		attachment: {
-			type: 'template',
-			payload: {
-				template_type: 'button',
-				text,
-				buttons: newCategories.map((c) => {
-					// Everything except the category name must be destructured
-					// for this to work
-					const { name: title, id: category_id } = c;
+  if (userLibraries.length) {
+    buttons.push({
+      type: 'postback',
+      title: 'View Library',
+      payload: JSON.stringify({ command: 'library' })
+    });
+  }
 
-					return {
-						type: 'postback',
-						title: title[0].toUpperCase() + title.substring(1),
-						payload: JSON.stringify({
-							command: 'pick_category',
-							category_id
-						})
-					};
-				})
-			}
-		}
-	});
+  buttons.push({
+    type: 'postback',
+    title: 'Get Help',
+    payload: JSON.stringify({ command: 'help' })
+  });
 
-	return response;
+  return [
+    {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'button',
+          text,
+          buttons
+        }
+      }
+    }
+  ];
 }
 
 async function getSingleBook(event, promise) {
-	const books = await promise;
-	const userInfo = await getUserInfo(event.sender, event.page.access_token);
+  const books = await promise;
+  const userInfo = await getUserInfo(event.sender, event.page.access_token);
 
-	const { id: book_id, title, author, synopsis, intro, image_url } = books[0];
+  const { id: book_id, title, author, synopsis, intro, image_url } = books[0];
 
-	const text = `Hi, ${userInfo.first_name}! ${intro}`;
+  const text = `Hi, ${userInfo.first_name}! ${intro}`;
 
-	const buttons = [];
-	if (synopsis) {
-		buttons.push({
-			type: 'postback',
-			title: 'Buy on Amazon',
-			payload: JSON.stringify({
-				command: 'buy_book',
-				book_id
-			})
-		});
-		buttons.push({
-			type: 'postback',
-			title: 'Quick Synopsis',
-			payload: JSON.stringify({
-				command: 'get_synopsis',
-				book_id
-			})
-		});
-	}
+  const buttons = [];
+  if (synopsis) {
+    buttons.push({
+      type: 'postback',
+      title: 'Quick Synopsis',
+      payload: JSON.stringify({
+        command: 'get_synopsis',
+        book_id
+      })
+    });
+  }
 
-	buttons.push({
-		type: 'postback',
-		title: 'Start Summary',
-		payload: JSON.stringify({
-			command: 'get_summary',
-			book_id
-		})
-	});
+  buttons.push({
+    type: 'postback',
+    title: 'Start Summary',
+    payload: JSON.stringify({
+      command: 'get_summary',
+      book_id
+    })
+  });
 
-	return [
-		{ text },
-		{
-			attachment: {
-				type: 'template',
-				payload: {
-					template_type: 'generic',
-					elements: [
-						{
-							title,
-							image_url,
-							subtitle: `by ${author}`,
-							buttons
-						}
-					]
-				}
-			}
-		}
-	];
+  return [
+    { text },
+    {
+      attachment: {
+        type: 'template',
+        payload: {
+          template_type: 'generic',
+          elements: [
+            {
+              title,
+              image_url,
+              subtitle: `by ${author}`,
+              buttons
+            }
+          ]
+        }
+      }
+    }
+  ];
 }
 
 /*
