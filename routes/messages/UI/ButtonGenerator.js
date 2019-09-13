a7be6b49e53e3b1e740d2d8874a7846a612d86e1
Erik Kimsey <kimseyerik@gmail.com>
2019-09-13 11:17:27 -0400

    Created ButtonGEnerator and GenericGenerator files.  Added preliminary documentation and structure to ButtonGenerator

---
+++ routes/messages/UI/ButtonGenerator.js
@@ -0,0 +1,6 @@
+/**
+ * Dynamically generates messenger buttons
+ * Params:
+ * - content: (object ? array) containing such as title, url, type, command
+ */
+module.exports = async (content) => {};
