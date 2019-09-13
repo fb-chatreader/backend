c1ba1d6261f4bd4c612a060bfa7613b56e698364
Erik Kimsey <kimseyerik@gmail.com>
2019-09-13 11:29:39 -0400

    Cleaned up ButtonGenerator

--- routes/messages/UI/ButtonGenerator.js
+++ routes/messages/UI/ButtonGenerator.js
@@ -1,41 +1,10 @@
 /**
  * Dynamically generates messenger buttons
  * Params:
- * - content: (object) containing:
- * {
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"generic",
-        "elements":[
-           {
-            "title":"Welcome!",
-            "image_url":"https://petersfancybrownhats.com/company_image.png",
-            "subtitle":"We have the right hat for everyone.",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://petersfancybrownhats.com/view?item=103",
-              "webview_height_ratio": "tall",
-            },
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://petersfancybrownhats.com",
-                "title":"View Website"
-              },{
-                "type":"postback",
-                "title":"Start Chatting",
-                "payload":"DEVELOPER_DEFINED_PAYLOAD"
-              }              
-            ]      
-          }
-        ]
-      }
-    }
-  }
+ * - content: (object) containing Button object values
  */
 module.exports = async (content) => {
-  const { type, payload, template_type, elements, title, image_url, subtitle, default_action, buttons } = content;
+  const { type, template_type, title, image_url, subtitle, default_action, buttons } = content;
   const { da_type, da_url, webview_height_ratio } = default_action;
 
   return {
