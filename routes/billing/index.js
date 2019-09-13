0fe09f381aaf14a5b1df5c57a3f7f0c686cb4e5b
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-09-13 14:17:46 -0500

    Created utils directory in root dir with functions for formatting stripe amounts to USD

--- routes/billing/index.js
+++ routes/billing/index.js
@@ -2,9 +2,30 @@ const router = require('express').Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 router.get('/productsandplans', async (req, res) => {
-    const products = await stripe.products.list({});
-    res.status(200).json(products);
+    const productsFromStripe = await stripe.products.list({});
+    const plansFromStripe = await stripe.plans.list({});
+    // get data from responses above, which contains array of products/plans:
+    const products = productsFromStripe.data;
+    const plans = plansFromStripe.data;
 
+    // Sort plans in ascending order of price (amount):
+    plans = plans.sort((a, b) => {
+        return a.amount - b.amount;
+    }).map(plan => {
+        // Format plan price (amount)
+        amount = UTILS.formatUSD(plan.amount)
+        return {...plan, amount};
+    });
+  
+    products.forEach(product => {
+        const filteredPlans = plans.filter(plan => {
+            return plan.product === product.id;
+        });
+
+        product.plans = filteredPlans;
+    });
+
+    // res.status(200).json(products);
 });
 
 module.exports = router;
