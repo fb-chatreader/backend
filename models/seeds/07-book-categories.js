ac0b7d14b2528bb76a20af8479e308b29fef8131
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-17 20:38:31 -0400

    Seeds correctly with the new quantity of books

--- models/seeds/07-book-categories.js
+++ models/seeds/07-book-categories.js
@@ -1,3 +1,20 @@
+const books = require('./allBooks/books.json');
+
 exports.seed = function(knex) {
-  return knex('book_categories').insert({ book_id: 1, category_id: 2 });
+  const categories = {};
+
+  books.forEach(({ category: c }) => {
+    if (!categories[c]) {
+      categories[c] = true;
+    }
+  });
+  // Objects aren't ordered so this is a little risky, but should work fine still
+  const categoryID = Object.keys(categories);
+
+  return knex('book_categories').insert(
+    books.map(({ category }, i) => ({
+      book_id: i + 1,
+      category_id: categoryID.indexOf(category) + 1
+    }))
+  );
 };
