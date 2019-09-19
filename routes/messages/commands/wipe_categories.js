66223132066d5225c9604e6c04b359fc67daa953
Erik Kimsey <kimseyerik@gmail.com>
2019-09-18 20:03:37 -0400

    Attempted wipe user_cat functionality.  Cleaned up logs and such.

--- routes/messages/commands/wipe_categories.js
+++ routes/messages/commands/wipe_categories.js
@@ -4,6 +4,8 @@ module.exports = async (event) => {
   const { user_id } = event;
   const userCategories = await UserCategories.retrieve({ user_id });
   userCategories.forEach((c) => {
-    UserCategories.remove({ user_id, c });
+    // console.log(c);
+    UserCategories.remove({ user_id, category_id: c.category_id });
   });
+  // console.log(userCategories);
 };
