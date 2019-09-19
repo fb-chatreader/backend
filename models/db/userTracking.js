a379768bebe6e27141af33cff3b27864989747b5
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-19 19:56:43 -0400

    Tracks user progress throughout the app

--- models/db/userTracking.js
+++ models/db/userTracking.js
@@ -1,14 +1,18 @@
-module.exports = { add, edit };
+module.exports = { retrieve, add, edit };
+
+function retrieve(filter) {
+  return filter ? db('user_tracking').where(filter) : db('user_tracking');
+}
 
 function add(userBook) {
   return db(`user_tracking`)
     .insert(userBook, ['*'])
-    .then(b => retrieve({ id: b[0].id }).first());
+    .then(u => retrieve({ id: u[0].id }).first());
 }
 
 function edit(filter, changes) {
   return db('user_tracking')
     .where(filter)
     .update(changes)
-    .then(b => retrieve({ id: b[0].id }).first());
+    .then(u => retrieve({ id: u[0].id }).first());
 }
