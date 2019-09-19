7872fa943442ad22c2b2d13e9a73cbbf1cac4fc3
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-09-18 21:25:29 -0700

    Deleted debuggin console logs from /newsub endpoint.

--- routes/billing/index.js
+++ routes/billing/index.js
@@ -43,7 +43,6 @@ router.post('/checkout/newsub', async (req, res) => {
     const customer = await stripe.customers.create({
         source: source           // source is the token.id created at checkout
     });
-    // console.log('Stripe customer response:', customer);
 
     // Create a Stripe charge and subscribe customer to the plan they chose:
     const subscription = await stripe.subscriptions.create({
@@ -52,18 +51,15 @@ router.post('/checkout/newsub', async (req, res) => {
             { plan: req.body.planID }
         ]      
     });
-    // console.log('Stripe subscription response:', subscription);
 
     const userUpdates = {
         stripe_customer_id: customer.id,
         stripe_subscription_id: subscription.id,
         stripe_subscription_status: subscription.status
     };
-    // console.log('userUpdates: ', userUpdates);
 
     const updatedUser = await Users.edit({ facebook_id }, userUpdates);
-    console.log('updatedUser: ', updatedUser);
-    
+
     res.status(201).json('Payment successful. Subscribed to plan.')   
 });
 
