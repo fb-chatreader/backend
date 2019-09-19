d2efb5e7fd237c71d968567e139d5fb4da30a8fa
Erik Kimsey <kimseyerik@gmail.com>
2019-09-19 11:33:44 -0400

    Moving branches

--- routes/messages/commands/pick_category.js
+++ routes/messages/commands/pick_category.js
@@ -3,15 +3,21 @@ const { getNewCategoriesForUser } = require('../helpers/categories.js');
 const QuickReplyTemplate = require('../UI/QuickReplyTemplate.js');
 
 module.exports = async (event) => {
-  const { user_id, category_id, command } = event;
-
+  const { user_id, category_id, command, isAdding } = event;
+  isAdding = isAdding;
   let text = null;
   let userCategories = await UserCategories.retrieve({ user_id });
   let categories = await getNewCategoriesForUser(user_id);
-  let userCatLength = await getUserCatLength(userCategories);
+  let userCatLength = userCategories.length;
+  // let userCatLength = await getUserCatLength(userCategories);
+  // let userCatLength = userCategories.length;
+  console.log('before >>>> userCategories.length <<<<<<< before');
+  console.log('before >>>> userCategories.length <<<<<<< before');
+  console.log('before >>>> userCategories.length <<<<<<< before');
+  console.log('before >>>> userCategories.length <<<<<<< before');
+  console.log(userCatLength);
   console.log('userCategories >>>> before');
   console.log(userCategories);
-  const isAdding = categories.length === 0 ? false : true;
   const isAddingParams = {
     isAdding,
     user_id,
@@ -20,22 +26,24 @@ module.exports = async (event) => {
     UserCategories
   };
 
-  handleIsAdding(isAddingParams);
+  if (isAdding) {
+    addCategory(isAddingParams);
+  }
 
   // If the user was sent here by another command, send them back as soon as they have
   // enough categories;
-  if (!isAdding || categories >= 3) {
-    return 'Done';
-  }
+  // if (isAdding === false || userCatLength >= 3) {
+  //   return 'Done';
+  // }
 
   // Currently categories are not tied to a page_id so we'd have to loop over their books or just add
   // a page_id to categories
-  console.log('userCategories.length <<<<<<< before');
-  console.log('userCategories.length <<<<<<< before');
-  console.log('userCategories.length <<<<<<< before');
-  console.log('userCategories.length <<<<<<< before');
   console.log(userCatLength);
-  if (userCatLength === 0) {
+  if (userCatLength < 1) {
+    console.log('userCategories.length <<<<<<< inside');
+    console.log('userCategories.length <<<<<<< inside');
+    console.log('userCategories.length <<<<<<< inside');
+    console.log(userCatLength);
     text = 'Tell us your top three favorite genres so we know what to suggest!  To get started pick your favorite!';
   } else if (userCatLength === 1) {
     text = 'Great, now pick a second!';
@@ -49,15 +57,11 @@ module.exports = async (event) => {
   categories = await getNewCategoriesForUser(user_id);
   const quickReplies = await QuickReplyTemplate(categories, event, userCategories);
 
-  userCategories = await UserCategories.retrieve({ user_id });
+  // userCategories = await UserCategories.retrieve({ user_id });
   console.log('userCategories >>>> after');
+  // userCatLength = await getUserCatLength(userCategories);
   console.log(userCategories);
-  userCatLength = await getUserCatLength(userCategories);
-  console.log('userCategories.length >>> after');
-  console.log('userCategories.length >>> after');
-  console.log('userCategories.length >>> after');
-  console.log('userCategories.length >>> after');
-  console.log(userCatLength);
+
   return [
     {
       text,
@@ -69,20 +73,33 @@ module.exports = async (event) => {
 function handleIsAdding(params) {
   const { isAdding, user_id, category_id, userCategories, UserCategories } = params;
   if (isAdding) {
-    return addCategory(user_id, category_id, userCategories, UserCategories);
+    addCategory(user_id, category_id, userCategories, UserCategories);
   }
   if (!isAdding) {
     removeCategory(user_id, category_id, userCategories, UserCategories);
   }
 }
 
-async function addCategory(user_id, category_id, userCategories, UserCategories) {
-  console.log('addCat');
+async function addCategory(params) {
+  const { isAdding, user_id, category_id, userCategories, UserCategories } = params;
   const newCategory =
     category_id && !userCategories.find((c) => c.category_id === category_id)
       ? await UserCategories.add({ user_id, category_id })
       : null;
   newCategory ? userCategories.push(newCategory) : null;
+
+  console.log('addCat');
+  console.log('addCat');
+  console.log(userCategories);
+  console.log('addCat');
+  console.log('addCat');
+  console.log('userCategories.length >>> after');
+  console.log('userCategories.length >>> after');
+  console.log('userCategories.length >>> after');
+  console.log('userCategories.length >>> after');
+  console.log(userCategories.length);
+  userCatLength = userCategories.length;
+  console.log(userCatLength);
 }
 
 async function removeCategory(user_id, category_id, userCategories, UserCategories) {
