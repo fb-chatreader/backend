d51d87d4599a58c3f1a1749f5971476a706fab3c
Erik Kimsey <kimseyerik@gmail.com>
2019-09-25 17:23:56 -0400

    Created generateRatings utility, utilizing create-json-file to populate ratings.json

--- routes/messages/commands/get_started.js
+++ routes/messages/commands/get_started.js
@@ -3,15 +3,17 @@ const getUserInfo = require('../helpers/getUserInfo.js');
 const UserCategories = require('models/db/userCategories.js');
 const UserLibraries = require('models/db/userLibraries.js');
 const GenericTemplate = require('routes/messages/UI/GenericTemplate.js');
-
-module.exports = async event => {
+const bookSearch = require('routes/books/helpers/book_search.js');
+// bookSearch();
+// const generateRatings = require('../../books/helpers/generateRatings');
+// getRatings();
+module.exports = async (event) => {
   const { bookCount } = event;
 
   if (!bookCount) {
     return [
       {
-        text:
-          'Sorry, this bot is still being created, please visit us again soon!'
+        text: 'Sorry, this bot is still being created, please visit us again soon!'
       }
     ];
   }
@@ -29,11 +31,7 @@ async function getMultipleBooks(event) {
     'Hi, welcome to Chatwise!  I can read book summaries to you to help you learn more about them before you buy! You can always say "get started" to come back to this message. Also,';
 
   const text =
-    userCategories.length === 0
-      ? `${firstTimeText} ${introText[0].toLowerCase()}${introText.substring(
-          1
-        )}`
-      : introText;
+    userCategories.length === 0 ? `${firstTimeText} ${introText[0].toLowerCase()}${introText.substring(1)}` : introText;
 
   const buttons = [
     {
