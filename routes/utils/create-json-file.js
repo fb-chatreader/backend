ac66ec22f16de6eab77a667ecbe084b0f2b13c73
Erik Kimsey <kimseyerik@gmail.com>
2019-09-25 10:15:57 -0400

    Fleshing out book_search.js using parts of  AJ's success algorithm

---
+++ routes/utils/create-json-file.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = '../../models/seeds/allBooks/ratings.json';
+
+const storeData = (data, path) => {
+  try {
+    fs.writeFileSync(path, JSON.stringify(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
