61ceaa869a61b08d58d11c971bcce51bd2f84215
Erik Kimsey <kimseyerik@gmail.com>
2019-09-25 10:15:57 -0400

    Fleshing out book_search.js using parts of  AJ's success algorithm

---
+++ routes/books/helpers/getRatings.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+  /**
+   * import axios
+   * import books.json
+   * get google URL
+   * google key
+   * loop thru books.json
+   * get book title (from books)
+   * get author name (from books)
+   * create GET url from google URL, title, author name,
+   * send request with URL
+   */
+};
