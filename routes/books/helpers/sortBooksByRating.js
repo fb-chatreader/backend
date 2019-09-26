60274701ce1d1af11d3844c45b8205408f9a1ffd
Erik Kimsey <kimseyerik@gmail.com>
2019-09-26 16:59:28 -0400

    sortBooks function called with category name, is able to sort category results by rating_qty

--- routes/books/helpers/sortBooksByRating.js
+++ routes/books/helpers/sortBooksByRating.js
@@ -1,7 +1,7 @@
 const Books = require('../../../models/db/books');
 const Categories = require('../../../models/db/categories');
 const BookCategories = require('../../../models/db/bookCategories');
-
+const getBooksFromCategory = require('../../../routes/messages/commands/get_books_from_category');
 /**
  * CONSIDERATIONS:
  * - sort as new books are added to allBooks?
@@ -9,53 +9,22 @@ const BookCategories = require('../../../models/db/bookCategories');
  */
 
 module.exports = async (event) => {
-  console.log('event');
-  console.log('event');
-  console.log('event');
   let category_id = null;
   const { category_name } = event;
-  // console.log(category_name);
-
-  const books = await Books.retrieve();
-  console.log('books');
-  console.log('books');
-  console.log('books');
-  console.log('books');
-  console.log(books);
-
   const categories = await Categories.retrieve();
-
-  /**
-   * get category_id of category_name
-   */
 
   categories.forEach((cat) => {
     if (cat.name === category_name) {
       category_id = cat.id;
     }
   });
+
   const catBooks = await BookCategories.retrieve({ category_id });
-  console.log('catBooks');
-  console.log('catBooks');
-  console.log('catBooks');
-  // console.log(catBooks);
-
-  const filteredBooks = await BookCategories.retrieve();
-
-  // const sortedBooks = books.map((book, i) => {
-  //   // console.log(category_id);
-  //   if (book.id === category_id) {
-  //     return book;
-  //   }
-  // });
-
-  // console.log(sortedBooks);
-
-  /**
-   * - @PARAMS category, array of books
-   * - filter out category from allBooks
-   * - sort filtered books by quantity
-   * - then, sort filtered books by avg_rating
-   * - 
-   */
+  console.log(sortRatingQty(catBooks));
 };
+
+function sortRatingQty(books) {
+  return books.sort((a, b) => {
+    return b.rating_qty - a.rating_qty;
+  });
+}
