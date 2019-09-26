9b4efde0ba2cc6ff4e35f4e619cb421e54ed631a
Erik Kimsey <kimseyerik@gmail.com>
2019-09-26 09:36:27 -0400

    Created sortBooksByRating.js. Included steps to complete functionality and some considerations regarding functinality

---
+++ routes/books/helpers/sortBooksByRating.js
@@ -0,0 +1,15 @@
+/**
+ * CONSIDERATIONS:
+ * - sort as new books are added to allBooks?
+ * - sort
+ */
+
+module.exports = () => {
+  /**
+   * - @PARAMS category, array of books
+   * - filter out category from allBooks
+   * - sort filtered books by quantity
+   * - then, sort filtered books by avg_rating
+   * - 
+   */
+};
