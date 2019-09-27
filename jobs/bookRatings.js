0fea1a68f47b68bdda2444d3dab5a60e455d4605
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-27 11:45:20 -0400

    Searches the Google Books API for book ratings

---
+++ jobs/bookRatings.js
@@ -0,0 +1,86 @@
+const axios = require('axios');
+const Books = require('models/db/books.js');
+const url = 'https://www.googleapis.com/books/v1/volumes?q=';
+const booksWithReviews = [];
+Books.retrieve()
+  .then(books => {
+    const counts = {
+      notFound: 0,
+      noRating: 0,
+      error: 0,
+      success: 0
+    };
+
+    books.forEach(async (b, i, arr) => {
+      // Add check for author name being one word
+      const author = b.author
+        .split(' ')[1]
+        .replace(/[^\w\s]/gi, '')
+        .toLowerCase();
+
+      console.log('AUTHOR: ', `${b.title} by ${author}`);
+
+      const fullURL = `${url}${b.title
+        .replace(/[^\w\s]/gi, '')
+        .toLowerCase()}+inauthor:${author}&key=${
+        process.env.GOOGLE_BOOKS_API_KEY
+      }`;
+
+      console.log(fullURL);
+      setTimeout(async () => {
+        try {
+          const res = await axios.get(fullURL);
+          let found = false;
+
+          for (let i = 0; i < res.data.items.length; i++) {
+            const { volumeInfo } = res.data.items[i];
+            const title = b && b.title ? b.title.toLowerCase() : null;
+            const volumeTitle =
+              volumeInfo && volumeInfo.title
+                ? volumeInfo.title.toLowerCase()
+                : null;
+
+            if (title && volumeTitle && title === volumeTitle) {
+              found = true;
+              const { averageRating, ratingsCount } = volumeInfo;
+              if (averageRating && ratingsCount) {
+                counts.success++;
+                booksWithReviews.push({
+                  ...b,
+                  avg_rating: averageRating,
+                  rating_qty: ratingsCount
+                });
+                console.log(
+                  `${b.title}: ${volumeInfo.averageRating} rating with ${volumeInfo.ratingsCount} reviews`
+                );
+              } else {
+                booksWithReviews.push({ ...b, avg_rating: 0, rating_qty: 0 });
+                console.log('No rating');
+                counts.noRating++;
+              }
+              break;
+            }
+          }
+          if (!found) {
+            booksWithReviews.push({ ...b, avg_rating: 0, rating_qty: 0 });
+            counts.notFound++;
+          }
+          found = false;
+        } catch (err) {
+          counts.error++;
+          console.log('ERROR ON: ', fullURL);
+        }
+        if (i === arr.length - 1) {
+          console.log('COUNTS: ', counts);
+          console.log('TOTAL: ', books.length);
+          Promise.all(
+            booksWithReviews.map(b => {
+              const { id, ...noID } = b;
+              Books.edit({ id: b.id }, noID);
+            })
+          );
+        }
+      }, 500 * i);
+    });
+  })
+  .catch(err => console.log('ERROR: ', err.response.data));
