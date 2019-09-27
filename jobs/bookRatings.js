b04693effaf141a922c935007698d6f9acfbf76d
Erik Kimsey <kimseyerik@gmail.com>
2019-09-27 16:06:51 -0400

    bookRatings functionality altered to account for monoyms

--- jobs/bookRatings.js
+++ jobs/bookRatings.js
@@ -2,86 +2,96 @@ const axios = require('axios');
 const Books = require('models/db/books.js');
 const url = 'https://www.googleapis.com/books/v1/volumes?q=';
 const booksWithReviews = [];
-Books.retrieve()
-  .then((books) => {
-    const counts = {
-      notFound: 0,
-      noRating: 0,
-      error: 0,
-      success: 0
-    };
 
-    function analyzeAuthorName(name) {
-      // check if [1] is null,
-      // if so, return [0] (if it is not null)
-      let author = name.split(' ').replace(/[^\w\s]/gi, '').toLowerCase();
-      console.log(author);
+function analyzeAuthorName(name) {
+  // check if [1] is null,
+  // if so, return [0] (if it is not null)
+  let authorArr = name.split(' ');
+  let cleanedArr = authorArr.map((e) => {
+    return e.replace(/[^\w\s]/gi, '').toLowerCase();
+  });
 
-      if (author !== null) {
-        return author;
-      }
-    }
+  if (cleanedArr[1] !== null && cleanedArr[1] !== undefined) {
+    return cleanedArr[1];
+  } else if (cleanedArr[0] !== null && cleanedArr[0] !== undefined) {
+    return cleanedArr[0];
+  }
+}
 
-    books.forEach(async (b, i, arr) => {
-      // Add check for author name being one word
-      // const author = b.author.split(' ')[1].replace(/[^\w\s]/gi, '').toLowerCase();
-      const author = b.author;
+function bookRatings() {
+  Books.retrieve()
+    .then((books) => {
+      const counts = {
+        notFound: 0,
+        noRating: 0,
+        error: 0,
+        success: 0
+      };
 
-      console.log('AUTHOR: ', `${b.title} by ${author}`);
+      books.forEach(async (b, i, arr) => {
+        // Add check for author name being one word
+        // const author = b.author.split(' ')[1].replace(/[^\w\s]/gi, '').toLowerCase();
+        const author = analyzeAuthorName(b.author);
 
-      const fullURL = `${url}${b.title.replace(/[^\w\s]/gi, '').toLowerCase()}+inauthor:${author}&key=${process.env
-        .GOOGLE_BOOKS_API_KEY}`;
+        // console.log('TITLE w/ AUTHOR: ', `${b.title} ..by.. ${author}`);
 
-      console.log(fullURL);
-      setTimeout(async () => {
-        try {
-          const res = await axios.get(fullURL);
-          let found = false;
+        const fullURL = `${url}${b.title.replace(/[^\w\s]/gi, '').toLowerCase()}+inauthor:${author}&key=${process.env
+          .GOOGLE_BOOKS_API_KEY}`;
 
-          for (let i = 0; i < res.data.items.length; i++) {
-            const { volumeInfo } = res.data.items[i];
-            const title = b && b.title ? b.title.toLowerCase() : null;
-            const volumeTitle = volumeInfo && volumeInfo.title ? volumeInfo.title.toLowerCase() : null;
+        console.log(fullURL);
+        setTimeout(async () => {
+          try {
+            const res = await axios.get(fullURL);
+            let found = false;
 
-            if (title && volumeTitle && title === volumeTitle) {
-              found = true;
-              const { averageRating, ratingsCount } = volumeInfo;
-              if (averageRating && ratingsCount) {
-                counts.success++;
-                booksWithReviews.push({
-                  ...b,
-                  avg_rating: averageRating,
-                  rating_qty: ratingsCount
-                });
-                console.log(`${b.title}: ${volumeInfo.averageRating} rating with ${volumeInfo.ratingsCount} reviews`);
-              } else {
-                booksWithReviews.push({ ...b, avg_rating: 0, rating_qty: 0 });
-                console.log('No rating');
-                counts.noRating++;
+            for (let i = 0; i < res.data.items.length; i++) {
+              const { volumeInfo } = res.data.items[i];
+              const title = b && b.title ? b.title.toLowerCase() : null;
+              const volumeTitle = volumeInfo && volumeInfo.title ? volumeInfo.title.toLowerCase() : null;
+
+              if (title && volumeTitle && title === volumeTitle) {
+                found = true;
+                const { averageRating, ratingsCount } = volumeInfo;
+                if (averageRating && ratingsCount) {
+                  counts.success++;
+                  booksWithReviews.push({
+                    ...b,
+                    avg_rating: averageRating,
+                    rating_qty: ratingsCount
+                  });
+                  console.log(`${b.title}: ${volumeInfo.averageRating} rating with ${volumeInfo.ratingsCount} reviews`);
+                } else {
+                  booksWithReviews.push({ ...b, avg_rating: 0, rating_qty: 0 });
+                  console.log('No rating');
+                  counts.noRating++;
+                }
+                break;
               }
-              break;
             }
+            if (!found) {
+              booksWithReviews.push({ ...b, avg_rating: 0, rating_qty: 0 });
+              counts.notFound++;
+            }
+            found = false;
+          } catch (err) {
+            counts.error++;
+            console.log('ERROR ON: ', fullURL);
           }
-          if (!found) {
-            booksWithReviews.push({ ...b, avg_rating: 0, rating_qty: 0 });
-            counts.notFound++;
+          if (i === arr.length - 1) {
+            // console.log('COUNTS: ', counts);
+            // console.log('TOTAL: ', books.length);
+            Promise.all(
+              booksWithReviews.map((b) => {
+                const { id, ...noID } = b;
+                Books.edit({ id: b.id }, noID);
+              })
+            );
           }
-          found = false;
-        } catch (err) {
-          counts.error++;
-          console.log('ERROR ON: ', fullURL);
-        }
-        if (i === arr.length - 1) {
-          console.log('COUNTS: ', counts);
-          console.log('TOTAL: ', books.length);
-          Promise.all(
-            booksWithReviews.map((b) => {
-              const { id, ...noID } = b;
-              Books.edit({ id: b.id }, noID);
-            })
-          );
-        }
-      }, 500 * i);
-    });
-  })
-  .catch((err) => console.log('ERROR: ', err.response.data));
+          console.log(counts);
+        }, 500 * i);
+      });
+    })
+    .catch((err) => console.log('ERROR: ', err.response.data));
+}
+
+bookRatings();
