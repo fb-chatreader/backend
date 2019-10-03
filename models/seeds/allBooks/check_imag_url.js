8b5782486970070eb62893c492cd99b79c7b96cb
Erik Kimsey <kimseyerik@gmail.com>
2019-10-03 16:01:05 -0400

    Used vim to replace all instanes of img_url in books.json

--- models/seeds/allBooks/check_imag_url.js
+++ models/seeds/allBooks/check_imag_url.js
@@ -7,11 +7,14 @@ let count = 0;
 ratings.forEach((e, i) => {
   if (e.rating_qty && e.avg_rating) {
     count++;
-    // console.log('QTY');
-    // console.log(e.rating_qty);
-    // console.log('AVG');
-    // console.log(e.avg_rating);
+
   }
 });
-console.log('count');
-console.log(count);
+books.forEach((e, i) => {
+  if (e.rating_qty && e.avg_rating) {
+    count++;
+
+  }
+});
+console.log('ratings');
+console.log(ratings.length);
--- routes/messages/helpers/getBooksInCategories.js
+++ routes/messages/helpers/getBooksInCategories.js
@@ -30,7 +30,7 @@ module.exports = async (user_id, category_id) => {
   }).first();
 
   const startSortedBookIndex = recommendedBookRecord ? recommendedBookRecord.current_sorted_book_index : 0;
-
+  
   const endSortedBookIndex = startSortedBookIndex + 10;
 
   const books = sortedBooks.slice(startSortedBookIndex, endSortedBookIndex);
