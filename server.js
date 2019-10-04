a2098b1c6dd90e5958db2dbc418ccda7e1646746
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-04 16:25:40 -0400

    Disables bookRatings

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const cors = require('cors');
 
 // Jobs -- Tasks running on a schedule
 require('./jobs/timedMessages.js');
-
-require('./jobs/bookRatings.js')();
+// require('./jobs/bookRatings.js')();
 
 // Import routes
 const messageRouter = require('./routes/messages/');
