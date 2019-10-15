3342e2df9d4097783e6c1904c42195cf698beca3
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-15 12:54:08 -0400

    Finishes gathering book summary info

--- models/seeds/allBooks/books.js
+++ models/seeds/allBooks/books.js
@@ -3,5 +3,6 @@ const b2 = require('./updated_books1.json');
 const b3 = require('./updated_books2.json');
 const b4 = require('./updated_books3.json');
 const b5 = require('./updated_books4.json');
+const b6 = require('./updated_books5.json');
 
-module.exports = [...b1, ...b2, ...b3, ...b4, ...b5];
+module.exports = [...b1, ...b2, ...b3, ...b4, ...b5, ...b6];
