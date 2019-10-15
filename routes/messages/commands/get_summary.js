fdb08b4140a51a7c2258900961accff665921d99
AJ Brush <ajbrush@gmail.com>
2019-10-15 15:16:24 -0400

    Adds subscription or credits check for book summaries

--- routes/messages/commands/get_summary.js
+++ routes/messages/commands/get_summary.js
@@ -3,23 +3,51 @@ const Books = require('models/db/books.js');
 const Summaries = require('models/db/summaryParts.js');
 const UserTracking = require('models/db/userTracking.js');
 const Users = require('models/db/users.js');
+const GenericTemplate = require('../UI/GenericTemplate.js');
 
 // Query database to get current summary location
 // If there isn't one, create it
 // Otherwise, increment and get next summary (check for end of book)
 
-module.exports = async (event) => {
+module.exports = async event => {
   if (event.type !== 'postback') return;
   // Collect needed data from DB
 
   const { user_id, book_id, user } = event;
 
+  const { stripe_subscription_status: isSubscribed, credits } = user;
+
   const chatRead = await ChatReads.retrieve({ user_id, book_id }).first();
+
   // Get the user's current chat read summary_id or if they don't have one,
   // Set to the current book's first summary_id
-
   let current_summary_id = chatRead ? chatRead.current_summary_id : null;
+
   if (!chatRead) {
+    // Before proceeding with a new book, verify the user is subscribed or has a credit
+    if (!isSubscribed && !credits) {
+      return [
+        GenericTemplate([
+          {
+            title: 'Please subscribe to continue reading more book summaries!',
+            image_url: 'https://i.imgur.com/UdZlgQA.png',
+            buttons: [
+              {
+                type: 'web_url',
+                url: process.env.FRONTEND_URL,
+                title: 'Subscribe'
+              }
+            ]
+          }
+        ])
+      ];
+    }
+
+    if (!isSubscribed) {
+      // If the account is not subscribed, decrement credits
+      await Users.edit({ id: user_id }, { credits: user.credits - 1 });
+    }
+
     const firstSummary = await Summaries.retrieve({ book_id }).first();
     current_summary_id = firstSummary.id;
 
@@ -32,6 +60,7 @@ module.exports = async (event) => {
       user_id,
       book_id
     }).first();
+
     !progressOnBook
       ? await UserTracking.add({
           user_id,
@@ -45,14 +74,17 @@ module.exports = async (event) => {
             repeat_count: progressOnBook.repeat_count + 1
           }
         );
-
-    // else if (!chatRead && user.credits === 0) {
-    //   return { text: 'Please, subscribe to read more summaries.' };
   } else {
     // It already exists and we just need to update the current summary being tracked
-    await UserTracking.edit({ user_id, book_id }, { last_summary_id: current_summary_id });
+    await UserTracking.edit(
+      { user_id, book_id },
+      { last_summary_id: current_summary_id }
+    );
   }
-  const summaries = await Summaries.retrieveBlock({ book_id }, current_summary_id);
+  const summaries = await Summaries.retrieveBlock(
+    { book_id },
+    current_summary_id
+  );
 
   // For the next round, update to the next summary_id (which will just be
   // the last id in the series if there are no more for the current book)
@@ -62,9 +94,6 @@ module.exports = async (event) => {
   // If not, just update the table with the new ID
   // Otherwise, create a new chat read for the user for this book
 
-  /**
-   * /// Begin -- AJ's Ternary Hell
-   */
   // chatRead
   //   ? summaries.isFinal
   //     ? await ChatReads.remove(chatRead.id)
@@ -74,15 +103,15 @@ module.exports = async (event) => {
   //       book_id,
   //       current_summary_id: next_summary_id
   //     });
-  /**
-   * /// End -- AJ's Ternary Hell
-   */
 
   if (chatRead) {
     if (summaries.isFinal) {
       await ChatReads.remove(chatRead.id);
     } else {
-      await ChatReads.edit({ user_id, book_id }, { current_summary_id: next_summary_id });
+      await ChatReads.edit(
+        { user_id, book_id },
+        { current_summary_id: next_summary_id }
+      );
     }
   } else if (!chatRead) {
     await ChatReads.add({
