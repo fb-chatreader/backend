8dc17893bf6a78be96a53bfd588fb66def101e12
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-16 14:47:29 -0400

    Completes handshake between frontend and backend

--- routes/billing/index.js
+++ routes/billing/index.js
@@ -1,91 +1,92 @@
 const router = require('express').Router();
 const UTILS = require('../utils/format-numbers.js');
 const Users = require('models/db/users.js');
-
+const getUserID = require('routes/messages/helpers/hashUserID.js');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // GET endpoint to retrieve all products and plans from Stripe:
-router.get('/productsandplans/:id', async (req, res) => {
-    const productsFromStripe = await stripe.products.list({});
-    const plansFromStripe = await stripe.plans.list({});
+router.get('/productsandplans/:token', async (req, res) => {
+  const { token } = req.params;
+  const user_id = getUserID({ token });
+  const productsFromStripe = await stripe.products.list({});
+  const plansFromStripe = await stripe.plans.list({});
+  // get data from responses above, which contains array of products/plans:
+  let products = productsFromStripe.data;
+  let plans = plansFromStripe.data;
 
-    // get data from responses above, which contains array of products/plans:
-    let products = productsFromStripe.data;
-    let plans = plansFromStripe.data;
+  // Get the user's subscripition id if they have one:
+  const user = await Users.retrieve({ id: user_id }).first();
 
-    // Get the user's subscripition id if they have one:
-    const facebook_id = req.params.id;
-    const user = await Users.retrieve({ facebook_id }).first();
-    
-    // Check if user has subscription id; if they do get the subscription plan id:
-    const subID = await user.stripe_subscription_id;
-    const subscription = subID ? await stripe.subscriptions.retrieve(subID) : null;
-    const currentPlanID = subscription ? subscription.plan.id : null;
-
-    // Sort plans in ascending order of price (amount):
-    plans = plans.sort((a, b) => {
-        return a.amount - b.amount;
-    }).map(plan => {
-        plan.user_is_subscribed = plan.id === currentPlanID ? true : false;
-        // Map to new array with formatted price (amount) for each plan:
-        amount = UTILS.formatUSD(plan.amount);
-        return {...plan, amount};
+  // Check if user has subscription id; if they do get the subscription plan id:
+  const subID = await user.stripe_subscription_id;
+  const subscription = subID
+    ? await stripe.subscriptions.retrieve(subID)
+    : null;
+  const currentPlanID = subscription ? subscription.plan.id : null;
+  // Sort plans in ascending order of price (amount):
+  plans = plans
+    .sort((a, b) => {
+      return a.amount - b.amount;
+    })
+    .map(plan => {
+      plan.user_is_subscribed = plan.id === currentPlanID ? true : false;
+      // Map to new array with formatted price (amount) for each plan:
+      amount = UTILS.formatUSD(plan.amount);
+      return { ...plan, amount };
     });
-  
-    // Attach each plan to its corresponding product:
-    products.forEach(product => {
-        const filteredPlans = plans.filter(plan => {
-            return plan.product === product.id;
-        });
-
-        product.plans = filteredPlans;
+  // Attach each plan to its corresponding product:
+  products.forEach(product => {
+    const filteredPlans = plans.filter(plan => {
+      return plan.product === product.id;
     });
 
-    res.status(201).json(products);
+    product.plans = filteredPlans;
+  });
+  console.log('FILTERED');
+  return res.status(201).json(products);
 });
 
 // POST endpoint to create a new subscription when a customer completes checkout:
 router.post('/checkout/newsub', async (req, res) => {
-    // POSSIBLE TO DO:
-        // check if user already has an active subscription
-    
-    const { facebook_id, source } = req.body;
+  // POSSIBLE TO DO:
+  // check if user already has an active subscription
 
-    // Create a customer with Stripe:
-    const customer = await stripe.customers.create({
-        source: source           // source is the token.id created at checkout
-    });
+  const { id_token, source, planID } = req.body;
+  const user_id = getUserID({ token: id_token });
 
-    // Create a Stripe charge and subscribe customer to the plan they chose:
-    const subscription = await stripe.subscriptions.create({
-        customer: customer.id,    // comes from create Customer call above
-        items: [
-            { plan: req.body.planID }
-        ]      
-    });
+  // Create a customer with Stripe:
+  const customer = await stripe.customers.create({
+    source: source // source is the token.id created at checkout
+  });
 
-    const userUpdates = {
-        stripe_customer_id: customer.id,
-        stripe_subscription_id: subscription.id,
-        stripe_subscription_status: subscription.status
-    };
+  // Create a Stripe charge and subscribe customer to the plan they chose:
+  const subscription = await stripe.subscriptions.create({
+    customer: customer.id, // comes from create Customer call above
+    items: [{ plan: planID }]
+  });
 
-    const updatedUser = await Users.edit({ facebook_id }, userUpdates);
+  const userUpdates = {
+    stripe_customer_id: customer.id,
+    stripe_subscription_id: subscription.id,
+    stripe_subscription_status: subscription.status
+  };
 
-    res.status(201).json('Payment successful. Subscribed to plan.');   
+  await Users.edit({ id: user_id }, userUpdates);
+
+  res.status(201).json('Payment successful. Subscribed to plan.');
 });
 
 router.post('/testuser', async (req, res) => {
-    console.log('testuser post endpoint hit');
-    const { email, facebook_id } = req.body;
-    const user = {
-        email,
-        facebook_id
-    };
+  console.log('testuser post endpoint hit');
+  const { email, facebook_id } = req.body;
+  const user = {
+    email,
+    facebook_id
+  };
 
-    const newUser = await Users.add(user);
+  const newUser = await Users.add(user);
 
-    res.status(201).json(newUser);
+  res.status(201).json(newUser);
 });
 
 module.exports = router;
