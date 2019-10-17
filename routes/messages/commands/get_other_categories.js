a7337aed536c6567f60642065f8839ca929eca4b
Cameron Ray <27529775+upsmancsr@users.noreply.github.com>
2019-10-17 14:19:24 -0700

    Added Other Categories quick reply to Browse replies. Created get_other_categories command to send other categories (those not saved in User Categories).

---
+++ routes/messages/commands/get_other_categories.js
@@ -0,0 +1,35 @@
+const UserCategories = require('models/db/userCategories.js');
+const Categories = require('models/db/categories.js');
+const QuickReplyTemplate = require('../UI/QuickReplyTemplate.js');
+
+module.exports = async event => {
+    const { user_id } = event;
+
+    const userCategories = await UserCategories.retrieve({ user_id });
+    const userCategoryIDs = userCategories.map(cat => {
+        return cat.category_id;
+    });
+
+    const allCategories = await Categories.retrieve();
+
+    const otherCategories = [];
+
+    allCategories.forEach(cat => {
+        if (!userCategoryIDs.includes(cat.id)) {
+            otherCategories.push(cat);
+        } 
+    });
+
+    const text = 'Here are some other categories';
+
+    const replies = otherCategories.map(({ id: category_id, name: title }) => ({
+        title,
+        payload: JSON.stringify({
+            command: 'get_books_from_category',
+            category_id
+        })
+    }));
+
+    return [QuickReplyTemplate(text, replies)];
+
+}
