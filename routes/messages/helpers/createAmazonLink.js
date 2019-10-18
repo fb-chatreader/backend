5fbae6107cda2084ec98966c46d97b92b5c19b27
ajb85 <ajbrush85@gmail.com>
2019-10-18 19:24:30 -0400

    Removes outdated code

--- routes/messages/helpers/createAmazonLink.js
+++
@@ -1,6 +0,0 @@
-// this helper function creates an amazon link from a chosen book title
-module.exports = (book) => {
-  const amazonPrefix = 'www.amazon.com/s?k=';
-  const amazonLinkArr = book.split(' ').join('+');
-  return amazonPrefix + amazonLinkArr;
-};
