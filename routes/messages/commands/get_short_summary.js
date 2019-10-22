b8f6c91c4e5151b40fc16544cfaa3c68e0c467a7
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-22 19:50:37 -0400

    Asks users to subscribe if they're out of tokens

--- routes/messages/commands/get_short_summary.js
+++ routes/messages/commands/get_short_summary.js
@@ -1,11 +1,18 @@
 const Books = require('models/db/books.js');
 
 const QRT = require('../UI/QuickReplyTemplate.js');
+const SubscribeTemplate = require('../UI/SubscribeTemplate.js');
 
 const getSummaryParts = require('../../books/helpers/getSummaryParts.js');
+const canUserReadBook = require('../helpers/canUserReadBook.js');
 
 module.exports = async event => {
-  const { user_id, book_id } = event;
+  const { book_id } = event;
+
+  const canRead = canUserReadBook(event);
+  if (!canRead) {
+    return [SubscribeTemplate({ ...event, command: 'start_book' })];
+  }
   const { shortSummary } = await Books.retrieve({ 'b.id': book_id }).first();
 
   const summaries = getSummaryParts(JSON.parse(shortSummary).join(' '));
