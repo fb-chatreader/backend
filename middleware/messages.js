d61f420a15766f0d07ff77030c4671c28b8a1f4d
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-28 18:22:17 -0400

    Adds diag logs

--- middleware/messages.js
+++ middleware/messages.js
@@ -4,21 +4,29 @@ const WebhookEvent = require('classes/WebhookEvent.js');
 
 module.exports = { validateWebhook };
 
-async function validateWebhook({ body: { entry } }, res, next) {
-  if (body.object === 'page' && entry && entry[0]) {
-    const { id } = entry[0];
-    const page = await Pages.retrieve({ id }).first();
-    if (!page) return res.sendStatus(404);
+async function validateWebhook({ body: { entry, object } }, res, next) {
+  console.log('RECEIVED MESSAGE', object, entry);
+  try {
+    if (object === 'page' && entry && entry[0]) {
+      const { id } = entry[0];
+      const page = await Pages.retrieve({ id }).first();
+      if (!page) return res.sendStatus(404);
 
-    // Initialize a new webhook event, set the page for it
-    const Event = new WebhookEvent();
-    Event.setPage(page);
-    const goodHook = await Event.processHook(entry[0]);
-    if (goodHook) {
-      entry[0].Event = Event;
-      next();
-    } else {
-      return res.sendStatus(400);
+      // Initialize a new webhook event, set the page for it
+      const Event = new WebhookEvent();
+      Event.setPage(page);
+      const goodHook = await Event.processHook(entry[0]);
+      if (goodHook) {
+        entry[0].Event = Event;
+        next();
+      } else {
+        return res.sendStatus(403);
+      }
     }
+  } catch (err) {
+    console.error('Something went wrong validating the webhook event: ', err);
+    // Sending a 500+ error seems to time FB sending webhook events out, which is a disaster
+    // for productivity on the bot.  So if something crashes, try to send a 400 instead.
+    return res.sendStatus(400);
   }
 }
