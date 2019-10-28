cde4ab4d1ee4bc2128e9549fe3da92fa14468d72
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-28 13:47:20 -0400

    Updates name to a more consistent pattern

--- middleware/messages.js
+++ middleware/messages.js
@@ -2,7 +2,7 @@ const Pages = require('models/db/pages.js');
 
 const WebhookEvent = require('classes/WebhookEvent.js');
 
-module.exports = { validateWebhook, getPageData, parseWebhook };
+module.exports = { validateWebhook };
 
 async function validateWebhook({ body: { entry } }, res, next) {
   if (body.object === 'page' && entry && entry[0]) {
@@ -15,6 +15,7 @@ async function validateWebhook({ body: { entry } }, res, next) {
     Event.setPage(page);
     const goodHook = await Event.processHook(entry[0]);
     if (goodHook) {
+      entry[0].Event = Event;
       next();
     } else {
       return res.sendStatus(400);
