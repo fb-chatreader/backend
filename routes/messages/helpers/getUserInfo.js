9a36e5400808ecfccec5d5189cd8c5bc48ff6fb5
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-28 16:14:21 -0400

    Refactors to use new class structure

--- routes/messages/helpers/getUserInfo.js
+++ routes/messages/helpers/getUserInfo.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 // Given a PSID and access token, Messenger can retrieve the user's public information
-module.exports = async (PSID, access_token) => {
-  const url = `https://graph.facebook.com/${PSID}?fields=first_name&access_token=${access_token}`;
+module.exports = async Event => {
+  const { sender_id, access_token } = Event;
+  const url = `https://graph.facebook.com/${sender_id}?fields=first_name&access_token=${access_token}`;
   try {
     const request = await axios.get(url);
     return request.data;
