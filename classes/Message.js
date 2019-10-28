9a23af8b4f671b3e568d6cb8016233b69017ff6a
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-10-28 11:23:56 -0400

    Updates class to handle all aspects of a message

--- classes/Message.js
+++ classes/Message.js
@@ -1,32 +1,75 @@
 const axios = require('axios');
+const reqDir = require('require-dir');
+const addTimedMessage = require('routes/messages/helpers/addTimedMessage.js');
 
 module.exports = class Message {
   constructor(event) {
+    this.templates = reqDir('routes/messages/UI/');
+    this.conditions = reqDir('routes/messages/conditions/');
+    this.db = reqDir('models/db/');
+    this.helpers = reqDir('routes/messages/helpers');
+
+    const { sender, page, command, user_id, ...additionalData } = event;
+
+    this.event = additionalData;
+    this.user_id = user_id;
+    this.command = command;
+    this.sender = sender;
+    this.page_id = page.id;
+    this.access_token = page.access_token;
+
+    this.willRespond = true; // Set to false if the command doesn't reply
+    this.override;
     this.response;
-    this.event = event;
-    this.sender = event.sender;
-    this.access_token = event.page.access_token;
+  }
+
+  execute() {}
+
+  overrideCommand(command) {
+    this.override = command;
+  }
+
+  async setConditions(...args) {
+    for (let i = 0; i < args.length; i++) {
+      const c = args[i];
+      if (!this.conditions[c]) {
+        console.error(`Warning: condition ${c} was not found.`);
+        continue;
+      }
+
+      const condition = this.conditions[c];
+
+      const passes = await condition.evaluate.call(this);
+      if (!passes) {
+        return await condition.action.call(this);
+      }
+    }
+  }
+
+  withDBs(...args) {
+    const dbs = [];
+
+    args.forEach(name => {
+      dbs.push(this.db[name]);
+    });
+
+    return dbs;
   }
 
   async respond() {
     if (!this.response) return;
-    // The response object must be an array.
-    // However it could:
-    // 1) Be a promise  --> this.response.then is truthy
-    // 2) Not be a promise --> this.response.then is falsey
-    // 3) Contain an array of promises --> this.response.then is falsey but this.response[0].then is truthy
-    if (this.response.then || this.response[0].then) {
-      const resolved = await this._resolvePromises();
-      if (!resolved) {
-        console.error(
-          `No response sent to user.  ${this.event.command} returned: ${resolved}`
-        );
-        return;
-      }
-      this._messageQueue(resolved);
-    } else {
-      this._messageQueue(this.response);
+    // If the response isn't a promise, make it one.
+    // Now we can always assume it's a promise.
+    this.response = Promise.resolve(this.response);
+
+    const resolved = await this._resolvePromises();
+    if (!resolved) {
+      console.error(
+        `No response sent to user.  ${this.command} returned: ${resolved}`
+      );
+      return;
     }
+    this._messageQueue(resolved);
   }
 
   _resolvePromises(promise = this.response) {
@@ -82,3 +125,72 @@ module.exports = class Message {
     }
   }
 };
+
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next;
+//     this.prev;
+//   }
+// }
+
+// class CommandQueue {
+//   constructor(command) {
+//     this.tail;
+//     this.head;
+//     this.original = command;
+//     this.length = 0;
+//   }
+
+//   push(...args) {
+//     // Can give this method any number of arguments and it'll insert them all in order
+//     // Each argument must be a string
+//     let lastNode = this.tail;
+//     args.forEach(command => {
+//       // Add each node to the tail end of the linked list
+//       const newNode = new Node(command);
+//       newNode.prev = lastNode;
+//       if (lastNode) {
+//         lastNode.next = newNode;
+//       } else {
+//         // This is the first insertion
+//         this.head = newNode;
+//       }
+//       this.tail = newNode;
+//       this.length++;
+//       lastNode = newNode;
+//     });
+//   }
+
+//   shift() {
+//     if (this.length) {
+//       // As long as the list has values, return the current head
+//       const firstNode = this.head;
+//       const secondNode = firstNode.next;
+//       if (secondNode) {
+//         // If this.head.next is undefined, skip this step
+//         secondNode.prev = undefined;
+//       }
+//       this.head = secondNode;
+//       this.length--;
+
+//       if (this.length === 0) {
+//         // If the last value was shifted, clear the tail value
+//         // The head should already be undefined
+//         this.tail = this.head;
+//       }
+//       return firstNode.value;
+//     }
+
+//     return null;
+//   }
+
+//   _returnOriginal() {
+//     if (!this.original) {
+//       return null;
+//     }
+//     const og = original;
+//     this.original = undefined;
+//     return og;
+//   }
+// }
