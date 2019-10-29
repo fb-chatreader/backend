0710159258c38b4a1e1ec044a51edfbe0ba79c66
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-28 20:16:29 -0400

    Refactors to use new structure

--- routes/messages/commands/library.js
+++ routes/messages/commands/library.js
@@ -1,66 +1,53 @@
-const UserLibrary = require('models/db/userLibraries.js');
-const Books = require('models/db/books.js');
 module.exports = async Event => {
-  const {
-    user_id,
-    page: { id: page_id }
-  } = Event;
+  const { user_id, page_id } = Event;
+  const [UserLibrary] = this.withDBs('userLibraries');
 
   const userLibrary = await UserLibrary.retrieve({ user_id, page_id });
 
   return !userLibrary.length
-    ? [
-        {
-          text: "You don't have a library yet!"
-        }
-      ]
-    : [
-        {
-          attachment: {
-            type: 'template',
-            payload: {
-              template_type: 'generic',
-              elements: userLibrary.slice(0, 10).map(b => {
-                const buttons = [];
-                if (b.synopsis) {
-                  buttons.push({
-                    type: 'postback',
-                    title: 'Read Synopsis',
-                    payload: JSON.stringify({
-                      command: 'get_synopsis',
-                      book_id: b.id
-                    })
-                  });
-                }
-                buttons.push(
-                  {
-                    type: 'postback',
-                    title: 'Start Summary',
-                    payload: JSON.stringify({
-                      command: 'get_summary',
-                      book_id: b.id
-                    })
-                  },
-                  {
-                    type: 'postback',
-                    title: 'Remove from Library',
-                    payload: JSON.stringify({
-                      command: 'toggle_in_library',
-                      book_id: b.id,
-                      isAdding: false
-                    })
-                  }
-                );
-
-                return {
-                  title: b.title,
-                  image_url: b.image_url,
-                  subtitle: `by ${b.author}`,
-                  buttons
-                };
+    ? {
+        text: "You don't have a library yet!"
+      }
+    : this.sendTemplate(
+        'Generic',
+        userLibrary.map(b => {
+          const buttons = [];
+          if (b.synopsis) {
+            buttons.push({
+              type: 'postback',
+              title: 'Read Synopsis',
+              payload: JSON.stringify({
+                command: 'get_synopsis',
+                book_id: b.id
+              })
+            });
+          }
+          buttons.push(
+            {
+              type: 'postback',
+              title: 'Start Summary',
+              payload: JSON.stringify({
+                command: 'get_summary',
+                book_id: b.id
+              })
+            },
+            {
+              type: 'postback',
+              title: 'Remove from Library',
+              payload: JSON.stringify({
+                command: 'toggle_in_library',
+                book_id: b.id,
+                isAdding: false
               })
             }
-          }
-        }
-      ];
+          );
+
+          return {
+            title: b.title,
+            image_url: b.image_url,
+            subtitle: `by ${b.author}`,
+            buttons
+          };
+        })
+      );
 };
