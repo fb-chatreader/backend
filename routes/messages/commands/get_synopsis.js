88eb7b57a8754ae0b6f4f96ed6e9f8a037913f1e
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-29 18:59:39 -0400

    Refactors to new class structure

--- routes/messages/commands/get_synopsis.js
+++ routes/messages/commands/get_synopsis.js
@@ -1,11 +1,10 @@
-module.exports = async Event => {
+module.exports = async function(Event) {
   const { book_id } = Event;
   const [Books] = this.withDBs('books');
 
   const book = await Books.retrieve({ 'b.id': book_id }).first();
   return this.sendTemplate('QuickReply', book.synopsis, [
     {
-      type: 'postback',
       title: 'Continue to Summary',
       payload: JSON.stringify({
         command: 'get_summary',
