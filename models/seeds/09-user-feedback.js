ba9850bc7307ab31fcc2135df44e9408aa89737b
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-11-08 11:51:36 -0500

    Fixes seeds

--- models/seeds/09-user-feedback.js
+++ models/seeds/09-user-feedback.js
@@ -3,13 +3,13 @@ const generateDummyUserData = require('../seeds/feedback/generateFeedback');
 exports.seed = function(knex) {
   const feedback = generateDummyUserData(5);
   return knex('user_feedback').insert(
-    feedback.map((datum) => {
+    feedback.map(datum => {
       const { feedback_score, additional_feedback, email } = datum;
 
       return {
-        feedback_score: feedback_score,
-        additional_feedback: additional_feedback,
-        email: email
+        score: feedback_score,
+        notes: additional_feedback,
+        email
       };
     })
   );
