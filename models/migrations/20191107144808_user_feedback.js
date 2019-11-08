313b596fe62b63ab97386416a7d6c1455ca94155
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-11-08 11:40:41 -0500

    Shortens column names

--- models/migrations/20191107144808_user_feedback.js
+++ models/migrations/20191107144808_user_feedback.js
@@ -1,10 +1,13 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('user_feedback', (tbl) => {
+  return knex.schema.createTable('user_feedback', tbl => {
     tbl.increments();
-    tbl.integer('feedback_score');
-    tbl.text('additional_feedback');
+    tbl.integer('score');
+    tbl.text('notes');
     tbl.text('email');
-    tbl.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    tbl
+      .timestamp('created_at')
+      .notNullable()
+      .defaultTo(knex.fn.now());
   });
 };
 
