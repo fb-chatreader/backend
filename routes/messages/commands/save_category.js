56343efe53d97c4b4d1f52115ade3af0b948ea60
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-11-11 13:39:39 -0500

    Adds diagnostic logs

--- routes/messages/commands/save_category.js
+++ routes/messages/commands/save_category.js
@@ -1,12 +1,15 @@
 module.exports = async function(Event) {
   const { user_id, category_id } = Event;
+  console.log('EVENT: ', event);
   const [UserCategories] = this.withDBs('userCategories');
-
+  console.log('DB: ', UserCategories);
   const userCategories = await UserCategories.retrieve({ user_id });
-
+  console.log('USER CATEGORIES: ', userCategories);
   if (!userCategories.find(c => c.category_id === category_id)) {
+    console.log('ADDING CATEGORY: ', category_id);
     await UserCategories.add({ user_id, category_id });
+    console.log('SAVED');
   }
-
+  console.log('ROUTING TO: ', Event.redirect);
   this.redirectTo(Event, Event.redirect);
 };
